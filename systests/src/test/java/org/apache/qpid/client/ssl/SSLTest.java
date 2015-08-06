908dc509fce89885a71f5cd83f30cf6b6a1da897
Robert Godfrey <rgodfrey@apache.org>
2015-08-06 14:56:33 +0000

    QPID-6682 : ensure correct protocol is used on SSLTest

--- systests/src/test/java/org/apache/qpid/client/ssl/SSLTest.java
+++ systests/src/test/java/org/apache/qpid/client/ssl/SSLTest.java
@@ -552,6 +552,7 @@ public class SSLTest extends QpidBrokerTestCase
             sslPortAttributes.put(Port.NAME, TestBrokerConfiguration.ENTRY_NAME_SSL_PORT);
             sslPortAttributes.put(Port.KEY_STORE, TestBrokerConfiguration.ENTRY_NAME_SSL_KEYSTORE);
             sslPortAttributes.put(Port.TRUST_STORES, Collections.singleton(TestBrokerConfiguration.ENTRY_NAME_SSL_TRUSTSTORE));
+            sslPortAttributes.put(Port.PROTOCOLS, System.getProperty(TEST_AMQP_PORT_PROTOCOLS_PROPERTY));
             getBrokerConfiguration().addObjectConfiguration(Port.class,sslPortAttributes);
 
             Map<String, Object> aliasAttributes = new HashMap<>();
