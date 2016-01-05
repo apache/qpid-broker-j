199b30815d3ad64a905f1acb79e11c9df0627478
Alex Rudyy <orudyy@apache.org>
2016-01-05 12:51:58 +0000

    QPID-6968: Fix setting of position in org.apache.qpid.bytebuffer.QpidByteBuffer.BufferDataInput#reset

--- common/src/main/java/org/apache/qpid/bytebuffer/QpidByteBuffer.java
+++ common/src/main/java/org/apache/qpid/bytebuffer/QpidByteBuffer.java
@@ -29,6 +29,7 @@ import java.nio.channels.GatheringByteChannel;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
@@ -900,7 +901,7 @@ public final class QpidByteBuffer
 
         public void reset()
         {
-            _buffer.position(_mark);
+            position(_mark);
         }
     }
 
