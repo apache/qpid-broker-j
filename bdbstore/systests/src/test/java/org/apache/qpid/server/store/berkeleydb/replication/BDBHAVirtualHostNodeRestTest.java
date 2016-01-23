1e43f3d6187c7f5b55d631ddb5d6e18bd64bf50e
Keith Wall <kwall@apache.org>
2016-01-23 20:12:54 +0000

    QPID-7009: [Java Broker] BDB HA - DbPing call should have an independent timeout control

--- bdbstore/systests/src/test/java/org/apache/qpid/server/store/berkeleydb/replication/BDBHAVirtualHostNodeRestTest.java
+++ bdbstore/systests/src/test/java/org/apache/qpid/server/store/berkeleydb/replication/BDBHAVirtualHostNodeRestTest.java
@@ -66,6 +66,7 @@ public class BDBHAVirtualHostNodeRestTest extends QpidRestTestCase
     public void setUp() throws Exception
     {
         setTestSystemProperty(ReplicatedEnvironmentFacade.REMOTE_NODE_MONITOR_INTERVAL_PROPERTY_NAME, "1000");
+        setTestSystemProperty(ReplicatedEnvironmentFacade.REMOTE_NODE_MONITOR_TIMEOUT_PROPERTY_NAME, "1000");
 
         super.setUp();
         _hostName = getTestName();
