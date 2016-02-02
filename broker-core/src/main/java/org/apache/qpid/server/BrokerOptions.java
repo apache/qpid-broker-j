8527883fe27ed08a5919cbd65b72045c7fae778b
Alex Rudyy <orudyy@apache.org>
2016-02-02 15:18:02 +0000

    QPID-7039: [Java Broker] Allow overriding of default initial configuration location via system property
               merged from trunk
               svn merge -c 1728089   https://svn.apache.org/repos/asf/qpid/java/trunk

--- broker-core/src/main/java/org/apache/qpid/server/BrokerOptions.java
+++ broker-core/src/main/java/org/apache/qpid/server/BrokerOptions.java
@@ -238,6 +238,11 @@ public class BrokerOptions
     {
         if(_initialConfigurationLocation == null)
         {
+            String overriddenDefaultConfigurationLocation = System.getProperty("qpid.initialConfigurationLocation");
+            if (overriddenDefaultConfigurationLocation != null)
+            {
+                return BrokerOptions.class.getClassLoader().getResource(overriddenDefaultConfigurationLocation).toExternalForm();
+            }
             return DEFAULT_INITIAL_CONFIG_LOCATION;
         }
 
