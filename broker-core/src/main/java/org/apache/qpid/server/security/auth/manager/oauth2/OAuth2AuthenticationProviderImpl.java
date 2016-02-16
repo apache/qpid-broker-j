a296ebc1a9462b1847370984b6be76d6d955b896
Keith Wall <kwall@apache.org>
2016-02-16 10:49:05 +0000

    QPID-7028: Address review comments from Rob Godfrey <rgodfrey@apache.org>
    Merged from trunk with command:
    
    svn merge -c 1730651 ^/qpid/java/trunk

--- broker-core/src/main/java/org/apache/qpid/server/security/auth/manager/oauth2/OAuth2AuthenticationProviderImpl.java
+++ broker-core/src/main/java/org/apache/qpid/server/security/auth/manager/oauth2/OAuth2AuthenticationProviderImpl.java
@@ -123,11 +123,11 @@ public class OAuth2AuthenticationProviderImpl
     protected void validateChange(final ConfiguredObject<?> proxyForValidation, final Set<String> changedAttributes)
     {
         super.validateChange(proxyForValidation, changedAttributes);
-        validateResolver((OAuth2AuthenticationProvider<?>)proxyForValidation);
-        validateSecureEndpoints((OAuth2AuthenticationProvider<?>)proxyForValidation);
-        validatePostLogoutURI(this);
+        final OAuth2AuthenticationProvider<?> validationProxy = (OAuth2AuthenticationProvider<?>) proxyForValidation;
+        validateResolver(validationProxy);
+        validateSecureEndpoints(validationProxy);
+        validatePostLogoutURI(validationProxy);
     }
-
 
     @Override
     public void onValidate()
