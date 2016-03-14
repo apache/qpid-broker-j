7161ad58bed8c68bc4e63043cd9fd765fa89e940
Robert Godfrey <rgodfrey@apache.org>
2016-03-14 16:41:48 +0000

    QPID-6969 : Address some review comments from [~lorenz.quack]

---
+++ broker-plugins/management-http/src/main/java/org/apache/qpid/server/management/plugin/servlet/query/ConfiguredObjectQuery.java
@@ -0,0 +1,184 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+package org.apache.qpid.server.management.plugin.servlet.query;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.qpid.filter.BooleanExpression;
+import org.apache.qpid.filter.Expression;
+import org.apache.qpid.server.model.ConfiguredObject;
+
+public final class ConfiguredObjectQuery
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfiguredObjectQuery.class);
+
+    private static final String[] STANDARD_FIELDS = new String[]{ConfiguredObject.ID, ConfiguredObject.NAME};
+
+    private final ConfiguredObjectExpressionFactory _expressionFactory = new ConfiguredObjectExpressionFactory();
+
+    private final List<List<Object>> _results;
+    private final List<String> _headers;
+
+    interface HeadersAndValueExpressions
+    {
+        List<String> getHeaders();
+        List<Expression> getValueExpressions();
+    }
+
+    public ConfiguredObjectQuery(List<ConfiguredObject<?>> objects, String selectClause, String whereClause)
+    {
+
+        HeadersAndValueExpressions headersAndValueExpressions = parseSelectClause(selectClause);
+
+        List<ConfiguredObject<?>> filteredObjects = filterObjects(objects, whereClause);
+
+        _headers = headersAndValueExpressions.getHeaders();
+        _results = evaluateResults(filteredObjects, headersAndValueExpressions.getValueExpressions());
+
+    }
+
+    public List<List<Object>> getResults()
+    {
+        return _results;
+    }
+
+    public List<String> getHeaders()
+    {
+        return _headers;
+    }
+
+    private HeadersAndValueExpressions parseSelectClause(final String selectClause)
+    {
+        final List<String> headers = new ArrayList<>();
+        final List<Expression> valueExpressions = new ArrayList<>();
+        if (selectClause != null)
+        {
+            ConfiguredObjectFilterParser parser = new ConfiguredObjectFilterParser();
+            parser.setConfiguredObjectExpressionFactory(_expressionFactory);
+
+            try
+            {
+                final List<Map<String, Expression>> expressions =
+                        parser.parseSelect(selectClause);
+                for (Map<String, Expression> expression : expressions)
+                {
+                    final Map.Entry<String, Expression> entry = expression.entrySet().iterator().next();
+                    headers.add(entry.getKey());
+                    valueExpressions.add(entry.getValue());
+                }
+            }
+            catch (ParseException | TokenMgrError e)
+            {
+                throw new QueryException("Unable to parse select clause");
+            }
+        }
+        else
+        {
+            for (String field : STANDARD_FIELDS)
+            {
+                headers.add(field);
+                valueExpressions.add(_expressionFactory.createConfiguredObjectExpression(field));
+            }
+        }
+        return new HeadersAndValueExpressions()
+                {
+                    @Override
+                    public List<String> getHeaders()
+                    {
+                        return headers;
+                    }
+
+                    @Override
+                    public List<Expression> getValueExpressions()
+                    {
+                        return valueExpressions;
+                    }
+                };
+    }
+
+    private List<ConfiguredObject<?>> filterObjects(final List<ConfiguredObject<?>> objects, final String whereClause)
+    {
+        List<ConfiguredObject<?>> filteredObjects = new ArrayList<>();
+        ConfiguredObjectFilterParser parser = new ConfiguredObjectFilterParser();
+        parser.setConfiguredObjectExpressionFactory(_expressionFactory);
+        try
+        {
+            final BooleanExpression<ConfiguredObject> expression =
+                    parser.parseWhere(whereClause);
+
+
+            for (ConfiguredObject<?> object : objects)
+            {
+                try
+                {
+                    if (expression.matches(object))
+                    {
+                        filteredObjects.add(object);
+                    }
+                }
+                catch (RuntimeException e)
+                {
+                    LOGGER.debug("Error while evaluating object against where clause", e);
+                }
+            }
+
+        }
+        catch (ParseException | TokenMgrError e)
+        {
+            throw new QueryException("Unable to parse where clause");
+        }
+        return filteredObjects;
+    }
+
+
+    private List<List<Object>> evaluateResults(final List<ConfiguredObject<?>> filteredObjects, List<Expression> valueExpressions)
+    {
+        List<List<Object>> values = new ArrayList<>();
+        for (ConfiguredObject<?> object : filteredObjects)
+        {
+            List<Object> objectVals = new ArrayList<>();
+            for (Expression<ConfiguredObject<?>> evaluator : valueExpressions)
+            {
+                Object value;
+                try
+                {
+                    value = evaluator.evaluate(object);
+                }
+                catch (RuntimeException e)
+                {
+                    LOGGER.debug("Error while evaluating select clause", e);
+                    value = null;
+                }
+                objectVals.add(value);
+            }
+            values.add(objectVals);
+        }
+        return values;
+    }
+
+
+
+}
--- broker-plugins/management-http/src/main/java/org/apache/qpid/server/management/plugin/servlet/rest/QueryServlet.java
+++ broker-plugins/management-http/src/main/java/org/apache/qpid/server/management/plugin/servlet/rest/QueryServlet.java
@@ -21,7 +21,6 @@
 package org.apache.qpid.server.management.plugin.servlet.rest;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,20 +32,15 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.qpid.filter.BooleanExpression;
-import org.apache.qpid.filter.Expression;
-import org.apache.qpid.server.management.plugin.HttpManagementUtil;
-import org.apache.qpid.server.management.plugin.servlet.query.ConfiguredObjectExpressionFactory;
-import org.apache.qpid.server.management.plugin.servlet.query.ConfiguredObjectFilterParser;
-import org.apache.qpid.server.management.plugin.servlet.query.ParseException;
-import org.apache.qpid.server.model.Broker;
+import org.apache.qpid.server.management.plugin.servlet.query.ConfiguredObjectQuery;
+import org.apache.qpid.server.management.plugin.servlet.query.QueryException;
 import org.apache.qpid.server.model.ConfiguredObject;
 import org.apache.qpid.server.model.Model;
 
 public abstract class QueryServlet<X extends ConfiguredObject<?>> extends AbstractServlet
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(QueryServlet.class);
-    private static final String[] STANDARD_FIELDS = new String[]{ConfiguredObject.ID, ConfiguredObject.NAME};
+
 
     @Override
     protected void doGetWithSubjectAndActor(HttpServletRequest request, HttpServletResponse response)
@@ -71,7 +65,6 @@ public abstract class QueryServlet<X extends ConfiguredObject<?>> extends Abstra
         if( parent != null && ((categoryName = getRequestedCategory(request)) != null))
         {
             Model model = parent.getModel();
-            ConfiguredObjectExpressionFactory expressionFactory = new ConfiguredObjectExpressionFactory();
 
             Class<? extends ConfiguredObject> category = getSupportedCategory(categoryName, model);
 
@@ -79,110 +72,25 @@ public abstract class QueryServlet<X extends ConfiguredObject<?>> extends Abstra
             {
                 List<ConfiguredObject<?>> objects = getAllObjects(parent, category, request);
                 Map<String, List<?>> resultsObject = new LinkedHashMap<>();
-                List<String> headers = new ArrayList<>();
-                List<Expression<ConfiguredObject<?>>> valueEvaluators = new ArrayList<>();
 
-                if (request.getParameterMap().containsKey("select"))
+                try
                 {
-                    ConfiguredObjectFilterParser parser = new ConfiguredObjectFilterParser();
-                    parser.setConfiguredObjectExpressionFactory(expressionFactory);
-                    String selectClause = request.getParameter("select");
+                    ConfiguredObjectQuery query = new ConfiguredObjectQuery(objects,
+                                                                            request.getParameter("select"),
+                                                                            request.getParameter("where"));
 
-                    try
-                    {
-                        final List<Map<String, Expression>> expressions =
-                                parser.parseSelect(selectClause);
-                        for (Map<String, Expression> expression : expressions)
-                        {
-                            final Map.Entry<String, Expression> entry = expression.entrySet().iterator().next();
-                            headers.add(entry.getKey());
-                            valueEvaluators.add(entry.getValue());
-                        }
-                    }
-                    catch (ParseException e)
-                    {
-                        sendJsonErrorResponse                               (request,
-                                                                             response,
-                                                                             HttpServletResponse.SC_NOT_FOUND,
-                                                                             "Unable to parse where clause");
-                        return;
-                    }
+                    resultsObject.put("headers", query.getHeaders());
+                    resultsObject.put("results", query.getResults());
+                    sendJsonResponse(resultsObject, request, response);
                 }
-                else
+                catch(QueryException e)
                 {
-                    for (String field : STANDARD_FIELDS)
-                    {
-                        headers.add(field);
-                        valueEvaluators.add(expressionFactory.createConfiguredObjectExpression(field));
-                    }
+                    sendJsonErrorResponse(request,
+                                          response,
+                                          HttpServletResponse.SC_NOT_FOUND,
+                                          e.getMessage());
                 }
-
-                if (request.getParameterMap().containsKey("where"))
-                {
-                    String whereClause = request.getParameter("where");
-
-                    ConfiguredObjectFilterParser parser = new ConfiguredObjectFilterParser();
-                    parser.setConfiguredObjectExpressionFactory(expressionFactory);
-                    try
-                    {
-                        final BooleanExpression<ConfiguredObject> expression =
-                                parser.parseWhere(whereClause);
-
-                        List<ConfiguredObject<?>> filteredObjects = new ArrayList<>();
-
-                        for (ConfiguredObject<?> object : objects)
-                        {
-                            try
-                            {
-                                if (expression.matches(object))
-                                {
-                                    filteredObjects.add(object);
-                                }
-                            }
-                            catch (RuntimeException e)
-                            {
-                                LOGGER.debug("Error while evaluating object against where clause", e);
-                            }
-                        }
-
-                        objects = filteredObjects;
-                    }
-                    catch (ParseException e)
-                    {
-                        sendJsonErrorResponse                               (request,
-                                                                             response,
-                                                                             HttpServletResponse.SC_NOT_FOUND,
-                                                                             "Unable to parse where clause");
-                        return;
-                    }
-
-                }
-
-                resultsObject.put("headers", headers);
-                List<List<Object>> values = new ArrayList<>();
-                for (ConfiguredObject<?> object : objects)
-                {
-                    List<Object> objectVals = new ArrayList<>();
-                    for (Expression<ConfiguredObject<?>> evaluator : valueEvaluators)
-                    {
-                        Object value;
-                        try
-                        {
-                            value = evaluator.evaluate(object);
-                        }
-                        catch (RuntimeException e)
-                        {
-                            LOGGER.debug("Error while evaluating select clause", e);
-                            value = null;
-                        }
-                        objectVals.add(value);
-                    }
-                    values.add(objectVals);
-                }
-                resultsObject.put("results", values);
-                sendJsonResponse(resultsObject, request, response);
             }
-
             else
             {
                 sendJsonErrorResponse(request,
@@ -208,4 +116,5 @@ public abstract class QueryServlet<X extends ConfiguredObject<?>> extends Abstra
     abstract protected List<ConfiguredObject<?>> getAllObjects(final X parent,
                                                                final Class<? extends ConfiguredObject> category,
                                                                final HttpServletRequest request);
+
 }
