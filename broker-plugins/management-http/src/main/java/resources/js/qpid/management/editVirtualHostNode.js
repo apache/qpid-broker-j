81b0013bbbb5fa12d33240aee435ef0e578c4d2a
Lorenz Quack <lquack@apache.org>
2016-05-04 13:34:01 +0000

    QPID-7211: [Java Broker, WMC] removed uninteded changes from previous commit

--- broker-plugins/management-http/src/main/java/resources/js/qpid/management/editVirtualHostNode.js
+++ broker-plugins/management-http/src/main/java/resources/js/qpid/management/editVirtualHostNode.js
@@ -1,3 +1,23 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
 define(["dojox/html/entities",
         "dojo/_base/array",
         "dojo/_base/event",
@@ -8,7 +28,6 @@ define(["dojox/html/entities",
         "dijit/registry",
         "dojo/parser",
         'dojo/json',
-        "dojo/promise/all",
         "dojo/query",
         "dojo/store/Memory",
         "dojo/data/ObjectStore",
@@ -34,7 +53,6 @@ define(["dojox/html/entities",
               registry,
               parser,
               json,
-              all,
               query,
               Memory,
               ObjectStore,
@@ -192,23 +210,3 @@ define(["dojox/html/entities",
 
         return virtualHostNodeEditor;
     });
-/*
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
- */
