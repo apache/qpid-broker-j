a2502096a12bf555d066bc31f569d19e9acc4161
Alex Rudyy <orudyy@apache.org>
2016-07-11 10:50:40 +0000

    QPID-7349: Change the hill climbing algorithm settings to allow test EndToEndTest.testHillClimbing to run in small number of iterations

--- qpid-perftests-systests/src/test/java/org/apache/qpid/systest/disttest/endtoend/EndToEndTest.java
+++ qpid-perftests-systests/src/test/java/org/apache/qpid/systest/disttest/endtoend/EndToEndTest.java
@@ -24,6 +24,7 @@ import static org.apache.qpid.disttest.ControllerRunner.HILL_CLIMBER_CONSUMPTION
 import static org.apache.qpid.disttest.ControllerRunner.HILL_CLIMBER_MINIMUM_DELTA;
 import static org.apache.qpid.disttest.ControllerRunner.HILL_CLIMBER_MAX_NUMBER_OF_RUNS;
 import static org.apache.qpid.disttest.ControllerRunner.HILL_CLIMBER_PRODUCTION_TO_TARGET_RATIO_SUCCESS_THRESHOLD;
+import static org.apache.qpid.disttest.ControllerRunner.HILL_CLIMBER_START_TARGET_RATE;
 import static org.apache.qpid.disttest.ControllerRunner.OUTPUT_DIR_PROP;
 import static org.apache.qpid.disttest.ControllerRunner.TEST_CONFIG_PROP;
 
@@ -171,10 +172,11 @@ public class EndToEndTest extends QpidBrokerTestCase
         arguments.put(OUTPUT_DIR_PROP, _outputDir.getAbsolutePath());
         arguments.put(HILL_CLIMB, "true");
         // Reduce the minimum delta and number of runs so that test executes more quickly.We are not interested in an accurate repeatable value in this test.
-        arguments.put(HILL_CLIMBER_MINIMUM_DELTA, "9999");
+        arguments.put(HILL_CLIMBER_MINIMUM_DELTA, "64000");
         arguments.put(HILL_CLIMBER_MAX_NUMBER_OF_RUNS, "1");
         arguments.put(HILL_CLIMBER_PRODUCTION_TO_TARGET_RATIO_SUCCESS_THRESHOLD, "0.01");
         arguments.put(HILL_CLIMBER_CONSUMPTION_TO_PRODUCTION_RATIO_SUCCESS_THRESHOLD, "0.01");
+        arguments.put(HILL_CLIMBER_START_TARGET_RATE, "131073");
 
         runController(arguments);
         checkXmlFile(arguments);
