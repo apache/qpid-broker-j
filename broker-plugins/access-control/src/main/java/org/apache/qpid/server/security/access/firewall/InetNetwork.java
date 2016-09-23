f39f215869caef34a838bc0428c8e8af47fa6234
Keith Wall <kwall@apache.org>
2016-09-23 15:28:59 +0000

    NO-JIRA: [Java Broker] Avoid compiler warning "java: _ used as an identifier (use of _ as an identifier might not be supported in releases after Java SE 8)"
    No functional change.

--- broker-plugins/access-control/src/main/java/org/apache/qpid/server/security/access/firewall/InetNetwork.java
+++ broker-plugins/access-control/src/main/java/org/apache/qpid/server/security/access/firewall/InetNetwork.java
@@ -103,7 +103,7 @@ class InetNetwork
                                         (byte) (mask[3] & ip[3])
                                 });
         }
-        catch (Exception _)
+        catch (Exception e)
         {
             return null;
         }
