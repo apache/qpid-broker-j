53cbe6f24427aa96825f78c645ca508af437c8d3
Keith Wall <kwall@apache.org>
2017-01-17 11:12:36 +0000

    QPID-7609: [System Tests/Integration Tests] Refactor the POMs to avoid the addition of the JMS 1.1 dependency if testing the Qpid JMS Client
    This works against both the released Qpid JMS Client (which has transitive dependency on JMS 1.1) and the current SNAPSHOT (which has transitive dependency on JMS 2.0).
    
    Also:
    
    * factored out a foreign message test from JMSPropertiesTest, removing the compile time JMS 1.1 dependency by using a proxy

--- systests/src/test/java/org/apache/qpid/test/unit/message/JMSPropertiesTest.java
+++ systests/src/test/java/org/apache/qpid/test/unit/message/JMSPropertiesTest.java
@@ -35,26 +35,14 @@ import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.Topic;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.apache.qpid.client.message.NonQpidObjectMessage;
 import org.apache.qpid.client.message.QpidMessageProperties;
 import org.apache.qpid.test.utils.QpidBrokerTestCase;
 
-/**
- * @author Apache Software Foundation
- */
 public class JMSPropertiesTest extends QpidBrokerTestCase
 {
-
-    private static final Logger _logger = LoggerFactory.getLogger(JMSPropertiesTest.class);
-
-    public static final String JMS_CORR_ID = "QPIDID_01";
-    public static final int JMS_DELIV_MODE = 1;
-    public static final String JMS_TYPE = "test.jms.type";
-    protected static final String NULL_OBJECT_PROPERTY = "NullObject";
-    protected static final String INVALID_OBJECT_PROPERTY = "InvalidObject";
+    private static final String JMS_CORR_ID = "QPIDID_01";
+    private static final String JMS_TYPE = "test.jms.type";
+    private static final String INVALID_OBJECT_PROPERTY = "InvalidObject";
 
     public void testJMSProperties() throws Exception
     {
@@ -65,15 +53,15 @@ public class JMSPropertiesTest extends QpidBrokerTestCase
         MessageConsumer consumer = consumerSession.createConsumer(queue);
 
         Connection con2 = getConnection();
-        Session producerSession = con2.createSession(false, Session.CLIENT_ACKNOWLEDGE);
-        MessageProducer producer = producerSession.createProducer(queue);
-        Destination JMS_REPLY_TO = createTestQueue(producerSession, "my.replyto");
+        final Session producerSession = con2.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        final MessageProducer producer = producerSession.createProducer(queue);
+        Destination replyTo = createTestQueue(producerSession, "my.replyto");
+
         // create a test message to send
-        ObjectMessage sentMsg = new NonQpidObjectMessage(producerSession);
+        ObjectMessage sentMsg = producerSession.createObjectMessage();
         sentMsg.setJMSCorrelationID(JMS_CORR_ID);
-        sentMsg.setJMSDeliveryMode(JMS_DELIV_MODE);
         sentMsg.setJMSType(JMS_TYPE);
-        sentMsg.setJMSReplyTo(JMS_REPLY_TO);
+        sentMsg.setJMSReplyTo(replyTo);
 
         String JMSXGroupID_VALUE = "group";
         sentMsg.setStringProperty("JMSXGroupID", JMSXGroupID_VALUE);
@@ -84,7 +72,7 @@ public class JMSPropertiesTest extends QpidBrokerTestCase
         try
         {
             sentMsg.setObjectProperty(INVALID_OBJECT_PROPERTY, new Exception());
-            fail("Non primitive Object Property value set");
+            fail("Non serializable Object Property value set");
         }
         catch (MessageFormatException mfe)
         {
@@ -100,17 +88,15 @@ public class JMSPropertiesTest extends QpidBrokerTestCase
         con.start();
 
         // get message and check JMS properties
-        ObjectMessage rm = (ObjectMessage) consumer.receive(2000);
+        ObjectMessage rm = (ObjectMessage) consumer.receive(getReceiveTimeout());
         assertNotNull(rm);
 
         assertEquals("JMS Correlation ID mismatch", sentMsg.getJMSCorrelationID(), rm.getJMSCorrelationID());
-        // TODO: Commented out as always overwritten by send delivery mode value - prob should not set in conversion
-        // assertEquals("JMS Delivery Mode mismatch",sentMsg.getJMSDeliveryMode(),rm.getJMSDeliveryMode());
         assertEquals("JMS Type mismatch", sentMsg.getJMSType(), rm.getJMSType());
         assertEquals("JMS Reply To mismatch", sentMsg.getJMSReplyTo(), rm.getJMSReplyTo());
         assertTrue("JMSMessageID Does not start ID:", rm.getJMSMessageID().startsWith("ID:"));
-        assertEquals("JMS Default priority should be 4",Message.DEFAULT_PRIORITY,rm.getJMSPriority());   
-        
+        assertEquals("JMS Default priority should be 4",Message.DEFAULT_PRIORITY,rm.getJMSPriority());
+
         //Validate that the JMSX values are correct
         assertEquals("JMSXGroupID is not as expected:", JMSXGroupID_VALUE, rm.getStringProperty("JMSXGroupID"));
         assertEquals("JMSXGroupSeq is not as expected:", JMSXGroupSeq_VALUE, rm.getIntProperty("JMSXGroupSeq"));
@@ -134,8 +120,8 @@ public class JMSPropertiesTest extends QpidBrokerTestCase
         assertTrue("JMSXGroupID not available.",JMSXGroupID_Available);
         assertTrue("JMSXGroupSeq not available.",JMSXGroupSeq_Available);
 
-        // Check that the NULL_OBJECT_PROPERTY was not set or transmitted.
-        assertFalse(NULL_OBJECT_PROPERTY + " was not set.", rm.propertyExists(NULL_OBJECT_PROPERTY));
+        // Check that the INVALID_OBJECT_PROPERTY was not set or transmitted.
+        assertFalse(INVALID_OBJECT_PROPERTY + " was not set.", rm.propertyExists(INVALID_OBJECT_PROPERTY));
 
         con.close();
     }
@@ -158,7 +144,7 @@ public class JMSPropertiesTest extends QpidBrokerTestCase
         m.setObjectProperty(QpidMessageProperties.AMQP_0_10_APP_ID, "my-app-id");
         prod.send(m);
 
-        Message msg = consumer.receive(1000);
+        Message msg = consumer.receive(getReceiveTimeout());
         assertNotNull(msg);
 
     	Enumeration<String> enu = msg.getPropertyNames();
