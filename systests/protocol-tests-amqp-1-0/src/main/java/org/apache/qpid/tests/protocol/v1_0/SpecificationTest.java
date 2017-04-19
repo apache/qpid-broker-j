932ba34eaf97a03e58eebcd37e79e5e52792b1e1
Lorenz Quack <lquack@apache.org>
2017-04-19 16:25:18 +0000

    QPID-7665: [Java Broker] Fix auto generated licenses in files.

--- systests/protocol-tests-amqp-1-0/src/main/java/org/apache/qpid/tests/protocol/v1_0/SpecificationTest.java
+++ systests/protocol-tests-amqp-1-0/src/main/java/org/apache/qpid/tests/protocol/v1_0/SpecificationTest.java
@@ -1,5 +1,4 @@
-package org.apache.qpid.tests.protocol.v1_0;/*
- *
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -16,8 +15,9 @@ package org.apache.qpid.tests.protocol.v1_0;/*
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *
  */
+
+package org.apache.qpid.tests.protocol.v1_0;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
