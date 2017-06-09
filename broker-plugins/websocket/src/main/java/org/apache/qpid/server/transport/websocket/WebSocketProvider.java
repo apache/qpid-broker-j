apache/qpid-broker-j

commit 529ed43b6d990204ee6e4acd11cd7843be03575d

QPID-7731: [HTTP Management] Upgrade Jetty to 9.4

broker-plugins/websocket/src/main/java/org/apache/qpid/server/transport/websocket/WebSocketProvider.java
@@ -21,13 +21,13 @@
 package org.apache.qpid.server.transport.websocket;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.lang.reflect.Field;
 import java.net.SocketAddress;
+import java.nio.ByteBuffer;
 import java.security.Principal;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -42,16 +42,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.eclipse.jetty.server.AbstractConnector;
+import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.eclipse.jetty.server.nio.SelectChannelConnector;
-import org.eclipse.jetty.server.ssl.SslSelectChannelConnector;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.util.thread.ThreadPool;
-import org.eclipse.jetty.websocket.WebSocket;
-import org.eclipse.jetty.websocket.WebSocketHandler;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.WebSocketAdapter;
+import org.eclipse.jetty.websocket.api.WebSocketPolicy;
+import org.eclipse.jetty.websocket.server.WebSocketHandler;
+import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -69,20 +72,21 @@ import org.apache.qpid.server.transport.SchedulingDelayNotificationListener;
 import org.apache.qpid.server.transport.ServerNetworkConnection;
 import org.apache.qpid.server.transport.network.Ticker;
 import org.apache.qpid.server.transport.network.security.ssl.SSLUtil;
-import org.apache.qpid.server.util.Action;
 import org.apache.qpid.server.util.ServerScopedRuntimeException;
+
 
 class WebSocketProvider implements AcceptingTransport
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketProvider.class);
-    public static final String AMQP_WEBSOCKET_SUBPROTOCOL = "AMQPWSB10";
-    public static final String X509_CERTIFICATES = "javax.servlet.request.X509Certificate";
+    private static final String AMQP_WEBSOCKET_SUBPROTOCOL = "amqp";
+
     private final Transport _transport;
     private final SSLContext _sslContext;
     private final AmqpPort<?> _port;
     private final Set<Protocol> _supported;
     private final Protocol _defaultSupportedProtocolReply;
     private final MultiVersionProtocolEngineFactory _factory;
+
     private Server _server;
 
     private final List<ConnectionWrapper> _activeConnections = new CopyOnWriteArrayList<>();
@@ -119,37 +123,26 @@ class WebSocketProvider implements AcceptingTransport
 
         _server = new Server();
 
-        final AbstractConnector connector;
-
+        final ServerConnector connector;
+        HttpConnectionFactory httpConnectionFactory = new HttpConnectionFactory();
+        httpConnectionFactory.getHttpConfiguration().setSendServerVersion(false);
+        httpConnectionFactory.getHttpConfiguration().setSendXPoweredBy(false);
 
         if (_transport == Transport.WS)
         {
-            connector = new SelectChannelConnector();
+            connector = new ServerConnector(_server, httpConnectionFactory);
         }
         else if (_transport == Transport.WSS)
         {
-            SslContextFactory factory = new SslContextFactory()
+            SslContextFactory sslContextFactory = new SslContextFactory()
                                         {
-                                            @Override
-                                            public String[] selectProtocols(String[] enabledProtocols, String[] supportedProtocols)
-                                            {
-                                                return SSLUtil.filterEnabledProtocols(enabledProtocols, supportedProtocols,
-                                                                                      _port.getTlsProtocolWhiteList(),
-                                                                                      _port.getTlsProtocolBlackList());
-                                            }
-
-                                            @Override
-                                            public String[] selectCipherSuites(String[] enabledCipherSuites, String[] supportedCipherSuites)
-                                            {
-                                                return SSLUtil.filterEnabledCipherSuites(enabledCipherSuites, supportedCipherSuites,
-                                                                                         _port.getTlsCipherSuiteWhiteList(),
-                                                                                         _port.getTlsCipherSuiteBlackList());
-                                            }
-
                                             @Override
                                             public void customize(final SSLEngine sslEngine)
                                             {
                                                 super.customize(sslEngine);
+                                                SSLUtil.updateEnabledCipherSuites(sslEngine, _port.getTlsCipherSuiteWhiteList(), _port.getTlsCipherSuiteBlackList());
+                                                SSLUtil.updateEnabledTlsProtocols(sslEngine, _port.getTlsProtocolWhiteList(), _port.getTlsProtocolBlackList());
+
                                                 if(_port.getTlsCipherSuiteWhiteList() != null
                                                    && !_port.getTlsCipherSuiteWhiteList().isEmpty())
                                                 {
@@ -159,20 +152,18 @@ class WebSocketProvider implements AcceptingTransport
                                                 }
                                             }
                                         };
-            factory.setSslContext(_sslContext);
+            sslContextFactory.setSslContext(_sslContext);
 
-            factory.setNeedClientAuth(_port.getNeedClientAuth());
-            factory.setWantClientAuth(_port.getWantClientAuth());
-            connector = new SslSelectChannelConnector(factory);
+            sslContextFactory.setNeedClientAuth(_port.getNeedClientAuth());
+            sslContextFactory.setWantClientAuth(_port.getWantClientAuth());
+            connector = new ServerConnector(_server, sslContextFactory, httpConnectionFactory);
         }
         else
         {
             throw new IllegalArgumentException("Unexpected transport on port " + _port.getName() + ":" + _transport);
         }
 
-        String bindingAddress = null;
-
-        bindingAddress = _port.getBindingAddress();
+        String bindingAddress = _port.getBindingAddress();
 
         if (bindingAddress != null && !bindingAddress.trim().equals("") && !bindingAddress.trim().equals("*"))
         {
@@ -185,30 +176,36 @@ class WebSocketProvider implements AcceptingTransport
         WebSocketHandler wshandler = new WebSocketHandler()
         {
             @Override
-            public WebSocket doWebSocketConnect(final HttpServletRequest request, final String protocol)
+            public void configure(final WebSocketServletFactory factory)
             {
+                factory.setCreator((req, resp) ->
+                                   {
+                                       resp.setAcceptedSubProtocol(AMQP_WEBSOCKET_SUBPROTOCOL);
+                                       return new AmqpWebSocket();
+                                   });
+            }
 
-                Certificate certificate = null;
-
-                if(Collections.list(request.getAttributeNames()).contains(X509_CERTIFICATES))
+            @Override
+            public void configurePolicy(final WebSocketPolicy policy)
+            {
+                super.configurePolicy(policy);
+                // It seems Jetty internally makes provision for maxBinaryMessageSize to be unconstrained, but the
+                // policy does not allow it to be configured it that way.
+                // See https://github.com/eclipse/jetty.project/issues/488
+                try
                 {
-                    X509Certificate[] certificates =
-                            (X509Certificate[]) request.getAttribute(X509_CERTIFICATES);
-                    if(certificates != null && certificates.length != 0)
-                    {
-
-                        certificate = certificates[0];
-                    }
+                    Field maxBinaryMessageSize = policy.getClass().getDeclaredField("maxBinaryMessageSize");
+                    maxBinaryMessageSize.setAccessible(true);
+                    maxBinaryMessageSize.set(policy, 0);
                 }
-
-                SocketAddress remoteAddress = new InetSocketAddress(request.getRemoteHost(), request.getRemotePort());
-                SocketAddress localAddress = new InetSocketAddress(request.getLocalName(), request.getLocalPort());
-                return new AmqpWebSocket(_transport, localAddress, remoteAddress, certificate, connector.getThreadPool());
+                catch (IllegalAccessException | NoSuchFieldException e)
+                {
+                    LOGGER.warn("Could not override maxBinaryMessageSize", e);
+                }
             }
         };
 
         _server.setHandler(wshandler);
-        _server.setSendServerVersion(false);
         wshandler.setHandler(new AbstractHandler()
         {
             @Override
@@ -248,37 +245,61 @@ class WebSocketProvider implements AcceptingTransport
     {
         _closed.set(true);
         _idleTimeoutChecker.wakeup();
+        try
+        {
+            _server.stop();
+        }
+        catch (Exception e)
+        {
+            LOGGER.warn("Error closing the web socket for : " +  _port.getPort(), e);
+            _server = null;
+        }
     }
 
     @Override
     public int getAcceptingPort()
     {
-        return _server == null || _server.getConnectors() == null || _server.getConnectors().length == 0 ? _port.getPort() : _server.getConnectors()[0].getLocalPort();
+        Server server = _server;
+        return server == null || server.getConnectors().length == 0 || !(server.getConnectors()[0] instanceof ServerConnector) ?
+                _port.getPort() :
+                ((ServerConnector) server.getConnectors()[0]).getPort();
     }
 
-    private class AmqpWebSocket implements WebSocket,WebSocket.OnBinaryMessage
+    public class AmqpWebSocket extends WebSocketAdapter
     {
-        private final SocketAddress _localAddress;
-        private final SocketAddress _remoteAddress;
-        private final Certificate _userCertificate;
-        private final ThreadPool _threadPool;
         private volatile MultiVersionProtocolEngine _protocolEngine;
         private volatile ConnectionWrapper _connectionWrapper;
 
-        private AmqpWebSocket(final Transport transport,
-                              final SocketAddress localAddress,
-                              final SocketAddress remoteAddress,
-                              final Certificate userCertificate,
-                              final ThreadPool threadPool)
+        @Override
+        public void onWebSocketConnect(final Session session)
         {
-            _localAddress = localAddress;
-            _remoteAddress = remoteAddress;
-            _userCertificate = userCertificate;
-            _threadPool = threadPool;
+            super.onWebSocketConnect(session);
+
+            SocketAddress localAddress = session.getLocalAddress();
+            SocketAddress remoteAddress = session.getRemoteAddress();
+            _protocolEngine = _factory.newProtocolEngine(remoteAddress);
+
+            // Let AMQP do timeout handling
+            session.setIdleTimeout(0);
+
+            _connectionWrapper = new ConnectionWrapper(session, localAddress, remoteAddress, _protocolEngine, _server.getThreadPool());
+            if (session.getUpgradeRequest() instanceof ServletUpgradeRequest)
+            {
+                ServletUpgradeRequest upgradeRequest = (ServletUpgradeRequest) session.getUpgradeRequest();
+                if (upgradeRequest.getCertificates() != null && upgradeRequest.getCertificates().length > 0)
+                {
+                    _connectionWrapper.setPeerCertificate(upgradeRequest.getCertificates()[0]);
+                }
+            }
+            _protocolEngine.setNetworkConnection(_connectionWrapper);
+            _protocolEngine.setWorkListener(object -> _server.getThreadPool().execute(() -> _connectionWrapper.doWork()));
+            _activeConnections.add(_connectionWrapper);
+            _idleTimeoutChecker.wakeup();
+
         }
 
         @Override
-        public void onMessage(final byte[] data, final int offset, final int length)
+        public void onWebSocketBinary(final byte[] data, final int offset, final int length)
         {
             synchronized (_connectionWrapper)
             {
@@ -311,41 +332,14 @@ class WebSocketProvider implements AcceptingTransport
         }
 
         @Override
-        public void onOpen(final Connection connection)
+        public void onWebSocketError(final Throwable cause)
         {
-
-            _protocolEngine = _factory.newProtocolEngine(_remoteAddress);
-
-            connection.setMaxBinaryMessageSize(0);
-
-            // Let AMQP do timeout handling
-            connection.setMaxIdleTime(0);
-
-            _connectionWrapper =
-                    new ConnectionWrapper(connection, _localAddress, _remoteAddress, _protocolEngine, _threadPool);
-            _connectionWrapper.setPeerCertificate(_userCertificate);
-            _protocolEngine.setNetworkConnection(_connectionWrapper);
-            _protocolEngine.setWorkListener(new Action<ProtocolEngine>()
-            {
-                @Override
-                public void performAction(final ProtocolEngine object)
-                {
-                    _threadPool.dispatch(new Runnable()
-                    {
-                        @Override
-                        public void run()
-                        {
-                            _connectionWrapper.doWork();
-                        }
-                    });
-                }
-            });
-            _activeConnections.add(_connectionWrapper);
-            _idleTimeoutChecker.wakeup();
+            super.onWebSocketError(cause);
+            LOGGER.error("onWebSocketError", cause);
         }
 
         @Override
-        public void onClose(final int closeCode, final String message)
+        public void onWebSocketClose(final int statusCode, final String reason)
         {
             _protocolEngine.closed();
             _activeConnections.remove(_connectionWrapper);
@@ -355,7 +349,7 @@ class WebSocketProvider implements AcceptingTransport
 
     private class ConnectionWrapper implements ServerNetworkConnection, ByteBufferSender
     {
-        private final WebSocket.Connection _connection;
+        private final Session _connection;
         private final SocketAddress _localAddress;
         private final SocketAddress _remoteAddress;
         private final ConcurrentLinkedQueue<QpidByteBuffer> _buffers = new ConcurrentLinkedQueue<>();
@@ -367,7 +361,7 @@ class WebSocketProvider implements AcceptingTransport
         private long _maxWriteIdleMillis;
         private long _maxReadIdleMillis;
 
-        public ConnectionWrapper(final WebSocket.Connection connection,
+        public ConnectionWrapper(final Session connection,
                                  final SocketAddress localAddress,
                                  final SocketAddress remoteAddress,
                                  final MultiVersionProtocolEngine protocolEngine,
@@ -493,7 +487,7 @@ class WebSocketProvider implements AcceptingTransport
         @Override
         public String getTransportInfo()
         {
-            return _connection.getProtocol();
+            return _connection.getProtocolVersion();
         }
 
         @Override
@@ -538,7 +532,7 @@ class WebSocketProvider implements AcceptingTransport
             {
                 try
                 {
-                    _connection.sendMessage(data, 0, size);
+                    _connection.getRemote().sendBytes(ByteBuffer.wrap(data));
                 }
                 catch (IOException e)
                 {
@@ -574,7 +568,7 @@ class WebSocketProvider implements AcceptingTransport
 
         public void tick()
         {
-            _threadPool.dispatch(_tickJob);
+            _threadPool.execute(_tickJob);
         }
     }
 

broker-core/src/main/java/org/apache/qpid/server/model/port/AmqpPortImpl.java
@@ -355,59 +355,7 @@ public class AmqpPortImpl extends AbstractClientAuthCapablePortWithAuthProvider<
                     + this.getName() + "' but no trust store defined");
         }
 
-        try
-        {
-            SSLContext sslContext = SSLUtil.tryGetSSLContext();
-
-            KeyManager[] keyManagers = keyStore.getKeyManagers();
-
-            TrustManager[] trustManagers;
-            if(trustStores == null || trustStores.isEmpty())
-            {
-                trustManagers = null;
-            }
-            else if(trustStores.size() == 1)
-            {
-                trustManagers = trustStores.iterator().next().getTrustManagers();
-            }
-            else
-            {
-                Collection<TrustManager> trustManagerList = new ArrayList<>();
-                final QpidMultipleTrustManager mulTrustManager = new QpidMultipleTrustManager();
-
-                for(TrustStore ts : trustStores)
-                {
-                    TrustManager[] managers = ts.getTrustManagers();
-                    if(managers != null)
-                    {
-                        for(TrustManager manager : managers)
-                        {
-                            if(manager instanceof X509TrustManager)
-                            {
-                                mulTrustManager.addTrustManager((X509TrustManager)manager);
-                            }
-                            else
-                            {
-                                trustManagerList.add(manager);
-                            }
-                        }
-                    }
-                }
-                if(!mulTrustManager.isEmpty())
-                {
-                    trustManagerList.add(mulTrustManager);
-                }
-                trustManagers = trustManagerList.toArray(new TrustManager[trustManagerList.size()]);
-            }
-            sslContext.init(keyManagers, trustManagers, null);
-
-            return sslContext;
-
-        }
-        catch (GeneralSecurityException e)
-        {
-            throw new IllegalArgumentException("Unable to create SSLContext for key or trust store", e);
-        }
+        return SSLUtil.createSslContext(keyStore, trustStores, getName());
     }
 
     private Protocol getDefaultAmqpSupportedReply()

broker-core/src/main/java/org/apache/qpid/server/model/port/HttpPort.java
@@ -23,6 +23,7 @@ package org.apache.qpid.server.model.port;
 import java.util.Set;
 
 import org.apache.qpid.server.model.AuthenticationProvider;
+import org.apache.qpid.server.model.DerivedAttribute;
 import org.apache.qpid.server.model.ManagedAttribute;
 import org.apache.qpid.server.model.ManagedContextDefault;
 import org.apache.qpid.server.model.ManagedObject;
@@ -38,6 +39,21 @@ public interface HttpPort<X extends HttpPort<X>> extends ClientAuthCapablePort<X
     String THREAD_POOL_MINIMUM = "threadPoolMinimum";
     String THREAD_POOL_MAXIMUM = "threadPoolMaximum";
     String ALLOW_CONFIDENTIAL_OPERATIONS_ON_INSECURE_CHANNELS = "allowConfidentialOperationsOnInsecureChannels";
+
+    String PORT_HTTP_NUMBER_OF_SELECTORS = "qpid.port.http.threadPool.numberOfSelectors";
+    @SuppressWarnings("unused")
+    @ManagedContextDefault(name = PORT_HTTP_NUMBER_OF_SELECTORS)
+    long DEFAULT_PORT_HTTP_NUMBER_OF_SELECTORS = -1;
+    String PORT_HTTP_NUMBER_OF_ACCEPTORS = "qpid.port.http.threadPool.numberOfAcceptors";
+
+    @SuppressWarnings("unused")
+    @ManagedContextDefault(name = PORT_HTTP_NUMBER_OF_ACCEPTORS)
+    long DEFAULT_PORT_HTTP_NUMBER_OF_ACCEPTORS = -1;
+
+    String PORT_HTTP_ACCEPT_BACKLOG = "qpid.port.http.acceptBacklog";
+    @SuppressWarnings("unused")
+    @ManagedContextDefault(name = PORT_HTTP_ACCEPT_BACKLOG)
+    int DEFAULT_PORT_HTTP_ACCEPT_BACKLOG = 1024;
 
     @ManagedAttribute(defaultValue = "*")
     String getBindingAddress();
@@ -80,16 +96,6 @@ public interface HttpPort<X extends HttpPort<X>> extends ClientAuthCapablePort<X
     @ManagedAttribute( defaultValue = "${" + PORT_HTTP_THREAD_POOL_MINIMUM + "}")
     int getThreadPoolMinimum();
 
-    String PORT_HTTP_ADDITIONAL_INTERNAL_THREADS = "port.http.additionalInternalThreads";
-    @SuppressWarnings("unused")
-    @ManagedContextDefault( name = PORT_HTTP_ADDITIONAL_INTERNAL_THREADS )
-    long DEFAULT_PORT_HTTP_ADDITIONAL_INTERNAL_THREADS = 5;
-
-    String PORT_HTTP_MAXIMUM_QUEUED_REQUESTS = "port.http.maximumQueuedRequests";
-    @SuppressWarnings("unused")
-    @ManagedContextDefault( name = PORT_HTTP_MAXIMUM_QUEUED_REQUESTS )
-    long DEFAULT_PORT_HTTP_MAXIMUM_QUEUED_REQUESTS = 1000;
-
     @ManagedAttribute( defaultValue = "false", description = "If true allow operations which may return confidential "
                                                              + "information to be executed on insecure connections")
     boolean isAllowConfidentialOperationsOnInsecureChannels();
@@ -98,4 +104,13 @@ public interface HttpPort<X extends HttpPort<X>> extends ClientAuthCapablePort<X
                                                             + "services for the broker, if no virtualhostalaias "
                                                             + "matches the HTTP Host in the request")
     boolean isManageBrokerOnNoAliasMatch();
+
+    @DerivedAttribute (description = "Number of acceptor threads")
+    int getNumberOfAcceptors();
+
+    @DerivedAttribute (description = "Number of selector threads")
+    int getNumberOfSelectors();
+
+    @DerivedAttribute (description = "Size of accepts backlog")
+    int getAcceptsBacklogSize();
 }

broker-core/src/main/java/org/apache/qpid/server/model/port/HttpPortImpl.java
@@ -20,25 +20,15 @@
  */
 package org.apache.qpid.server.model.port;
 
-import java.security.PrivilegedAction;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-
-import javax.security.auth.Subject;
 
 import org.apache.qpid.server.configuration.IllegalConfigurationException;
 import org.apache.qpid.server.model.ConfiguredObject;
 import org.apache.qpid.server.model.Container;
-import org.apache.qpid.server.model.HostNameAlias;
 import org.apache.qpid.server.model.ManagedAttributeField;
 import org.apache.qpid.server.model.ManagedObjectFactoryConstructor;
-import org.apache.qpid.server.model.PatternMatchingAlias;
 import org.apache.qpid.server.model.State;
-import org.apache.qpid.server.model.SystemAddressSpaceAlias;
-import org.apache.qpid.server.model.VirtualHostAlias;
 import org.apache.qpid.server.util.PortUtil;
 
 public class HttpPortImpl extends AbstractClientAuthCapablePortWithAuthProvider<HttpPortImpl> implements HttpPort<HttpPortImpl>
@@ -59,6 +49,9 @@ public class HttpPortImpl extends AbstractClientAuthCapablePortWithAuthProvider<
 
     @ManagedAttributeField
     private boolean _manageBrokerOnNoAliasMatch;
+    private int _numberOfAcceptors;
+    private int _numberOfSelectors;
+    private int _acceptsBacklogSize;
 
     @ManagedObjectFactoryConstructor
     public HttpPortImpl(final Map<String, Object> attributes,
@@ -109,6 +102,44 @@ public class HttpPortImpl extends AbstractClientAuthCapablePortWithAuthProvider<
     }
 
     @Override
+    public int getNumberOfAcceptors()
+    {
+        return _numberOfAcceptors;
+    }
+
+    @Override
+    public int getNumberOfSelectors()
+    {
+        return _numberOfSelectors;
+    }
+
+    @Override
+    public int getAcceptsBacklogSize()
+    {
+        return _acceptsBacklogSize;
+    }
+
+    @Override
+    protected void onOpen()
+    {
+        super.onOpen();
+
+        int maxThreads = getThreadPoolMaximum();
+        int min = Math.max(1, Math.min(4, maxThreads / 2 - 1));
+        _acceptsBacklogSize = getContextValue(Integer.class, HttpPort.PORT_HTTP_ACCEPT_BACKLOG);
+        _numberOfAcceptors = getContextValue(Integer.class, HttpPort.PORT_HTTP_NUMBER_OF_ACCEPTORS);
+        if (_numberOfAcceptors < 0) // if _numberOfAcceptors == 0, selectors threads are used as acceptors
+        {
+            _numberOfAcceptors = Math.max(1, Math.min(min, Runtime.getRuntime().availableProcessors() / 8));
+        }
+        _numberOfSelectors = getContextValue(Integer.class, HttpPort.PORT_HTTP_NUMBER_OF_SELECTORS);
+        if (_numberOfSelectors <= 0)
+        {
+            _numberOfSelectors =  Math.max(1, Math.min(min, Runtime.getRuntime().availableProcessors() / 2));
+        }
+    }
+
+    @Override
     protected State onActivate()
     {
         if(_portManager != null && _portManager.isActivationAllowed(this))
@@ -127,16 +158,10 @@ public class HttpPortImpl extends AbstractClientAuthCapablePortWithAuthProvider<
         super.onValidate();
         validateThreadPoolSettings(this);
 
-        final double additionalInternalThreads = getContextValue(Integer.class, HttpPort.PORT_HTTP_ADDITIONAL_INTERNAL_THREADS);
-        if (additionalInternalThreads < 1)
+        final double acceptsBacklogSize = getContextValue(Integer.class, HttpPort.PORT_HTTP_ACCEPT_BACKLOG);
+        if (acceptsBacklogSize < 1)
         {
-            throw new IllegalConfigurationException(String.format("Number of additional internal threads %d is too small. Must be greater than zero.", additionalInternalThreads));
-        }
-
-        final double maximumQueuedRequests = getContextValue(Integer.class, HttpPort.PORT_HTTP_MAXIMUM_QUEUED_REQUESTS);
-        if (maximumQueuedRequests < 1)
-        {
-            throw new IllegalConfigurationException(String.format("Number of additional internal threads %d is too small. Must be greater than zero.", maximumQueuedRequests));
+            throw new IllegalConfigurationException(String.format("The size of accepts backlog %d is too small. Must be greater than zero.", acceptsBacklogSize));
         }
     }
 

commit f630b9ef9a67f77481ca0b128a10875c34a976d2

QPID-7731: Return the bound port rather than configured port number (fixing defect introduced by 529ed43)

broker-plugins/websocket/src/main/java/org/apache/qpid/server/transport/websocket/WebSocketProvider.java
@@ -350,7 +350,7 @@ class WebSocketProvider implements AcceptingTransport
         Server server = _server;
         return server == null || server.getConnectors().length == 0 || !(server.getConnectors()[0] instanceof ServerConnector) ?
                 _port.getPort() :
-                ((ServerConnector) server.getConnectors()[0]).getPort();
+                ((ServerConnector) server.getConnectors()[0]).getLocalPort();
     }
 
     public class AmqpWebSocket extends WebSocketAdapter