apache/qpid-broker-j

commit 6bde48c519847a14dcdbeba33b9eb200f3a089cd

QPID-7789: Refactor SASL web authentication

broker-plugins/management-http/src/main/java/org/apache/qpid/server/management/plugin/HttpManagement.java
@@ -345,7 +345,6 @@ public class HttpManagement extends AbstractPluginAdapter<HttpManagement> implem
         root.addFilter(restAuthorizationFilter, "/apidocs/*", EnumSet.of(DispatcherType.REQUEST));
         root.addFilter(restAuthorizationFilter, "/service/*", EnumSet.of(DispatcherType.REQUEST));
 
-        root.addFilter(new FilterHolder(new RedirectingFilter()), "/index.html", EnumSet.of(DispatcherType.REQUEST));
         root.addFilter(new FilterHolder(new RedirectingFilter()), "/", EnumSet.of(DispatcherType.REQUEST));
         if (_serveUncompressedDojo)
         {

broker-plugins/management-http/src/main/java/org/apache/qpid/server/management/plugin/auth/UsernamePasswordInteractiveLogin.java
@@ -34,11 +34,11 @@ import org.apache.qpid.server.security.auth.manager.UsernamePasswordAuthenticati
 @PluggableService
 public class UsernamePasswordInteractiveLogin implements HttpRequestInteractiveAuthenticator
 {
-    // TODO: When we refactor web management and adopt web fragments, move login.html (and logout.html)
+    // TODO: When we refactor web management and adopt web fragments, move logout.html
     // to WEB-INF/ and dispatch (forward) to them, rather than using a client side redirect.
     // This would keep the login/logout pages private and inaccessible to the user when using auth providers
     // such as Ouath2.
-    private static final String DEFAULT_LOGIN_URL = "/login.html";
+    private static final String DEFAULT_LOGIN_URL = "/index.html";
 
     private static final AuthenticationHandler REDIRECT_HANDLER = new AuthenticationHandler()
     {

broker-plugins/management-http/src/main/java/org/apache/qpid/server/management/plugin/servlet/rest/SaslServlet.java
@@ -256,7 +256,7 @@ public class SaslServlet extends AbstractServlet
                 HttpManagementUtil.saveAuthorisedSubject(request, subject);
                 if(challenge != null && challenge.length != 0)
                 {
-                    outputObject.put("challenge", DatatypeConverter.printBase64Binary(challenge));
+                    outputObject.put("additionalData", DatatypeConverter.printBase64Binary(challenge));
                 }
                 responseStatus = HttpServletResponse.SC_OK;
             }

broker-plugins/management-http/src/main/java/resources/common/LoginForm.html
@@ -1,0 +1,47 @@
+<!--
+  ~ Licensed to the Apache Software Foundation (ASF) under one
+  ~ or more contributor license agreements.  See the NOTICE file
+  ~ distributed with this work for additional information
+  ~ regarding copyright ownership.  The ASF licenses this file
+  ~ to you under the Apache License, Version 2.0 (the
+  ~ "License"); you may not use this file except in compliance
+  ~ with the License.  You may obtain a copy of the License at
+  ~
+  ~   http://www.apache.org/licenses/LICENSE-2.0
+  ~
+  ~ Unless required by applicable law or agreed to in writing,
+  ~ software distributed under the License is distributed on an
+  ~ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  ~ KIND, either express or implied.  See the License for the
+  ~ specific language governing permissions and limitations
+  ~ under the License.
+  -->
+<div>
+    <div data-dojo-type="dijit.TitlePane" data-dojo-props="title:'Login', toggleable: false">
+        <div data-dojo-type="dijit.form.Form" method="POST" data-dojo-attach-point="formWidget">
+            <div class="dijitDialogPaneContentArea">
+                <div class="errorDialogErrorPrimary">
+                    <span data-dojo-attach-point="statusCodeNode"></span> <span
+                        data-dojo-attach-point="errorMessageNode"></span>
+                </div>
+                <div data-dojo-type="dojox.layout.TableContainer"
+                     data-dojo-props="cols:1,labelWidth:'100',showLabels:true,orientation:'horiz',customClass:'formLabel'">
+                    <div data-dojo-type="dijit.form.ValidationTextBox"
+                         data-dojo-attach-point="usernameWidget"
+                         data-dojo-props="label:'User name:',required:true, intermediateChanges:true"></div>
+                    <div data-dojo-type="dijit.form.ValidationTextBox"
+                         type="password"
+                         data-dojo-attach-point="passwordWidget"
+                         data-dojo-props="label:'Password:',required:true, intermediateChanges:true"></div>
+                </div>
+            </div>
+            <div class="dijitDialogPaneActionBar qpidDialogPaneActionBar">
+                <button data-dojo-type="dijit.form.Button"
+                        type="submit"
+                        data-dojo-attach-point="loginButtonWidget"
+                        data-dojo-props="disabled:true">Login
+                </button>
+            </div>
+        </div>
+    </div>
+</div>

broker-plugins/management-http/src/main/java/resources/css/common.css
@@ -86,7 +86,7 @@ h1 {
 	font-size:1.5em;
 }
 
-#header {
+.header {
     height:100px;
     background:url("../images/qpid-logo.png") left center no-repeat;
     float: left;
@@ -748,4 +748,8 @@ td.advancedSearchField, col.autoWidth {
 .claro .loading
 {
     background-image: url("../dojo/dijit/themes/claro/images/loadingAnimation.gif");
+}
+.loadingBar
+{
+    text-align: center;
 }

broker-plugins/management-http/src/main/java/resources/index.html
@@ -91,6 +91,7 @@
                     "qpid/common/ConsoleHelper",
                     "qpid/management/treeView",
                     "qpid/management/controller",
+                    "qpid/common/LoginForm",
                     "dijit/layout/BorderContainer",
                     "dijit/layout/TabContainer",
                     "dijit/layout/ContentPane",
@@ -114,80 +115,102 @@
 </head>
 <body class="claro qpid">
 
-<div id="pageLayout" data-dojo-type="dijit.layout.BorderContainer"
-     data-dojo-props="design: 'headline', gutters: false, isLayoutContainer:false">
-    <div data-dojo-type="dijit.layout.ContentPane" data-dojo-props="region:'top', isLayoutContainer:false">
-        <div id="header"></div>
-        <div class="alignRight">
-            <div id="login" class="hidden" data-dojo-type="dijit/Toolbar">
-                <div data-dojo-type="dijit/form/DropDownButton" data-dojo-props="iconClass: 'dijitIconChart'">
-                    <span>Dashboard</span>
-                    <div data-dojo-type="dijit/Menu">
-                        <div data-dojo-type="dijit/MenuItem"
-                             data-dojo-props="onClick: function(e){qpidManagementHelper.showDashboardCreateDialog(e);}">
-                            Create
-                        </div>
-                        <div data-dojo-type="dijit/MenuItem"
-                             data-dojo-props="onClick: function(e){qpidManagementHelper.showDashboardBrowser(e);}">
-                            Browse
-                        </div>
-                    </div>
-                </div>
-                <div data-dojo-type="dijit/form/DropDownButton" data-dojo-props="iconClass: 'dijitIconApplication'">
-                    <span>Query</span>
-                    <div data-dojo-type="dijit/Menu">
-                        <div data-dojo-type="dijit/MenuItem"
-                             data-dojo-props="onClick: function(e){qpidManagementHelper.showQueryCreateDialog(e);}">
-                            Create
-                        </div>
-                        <div data-dojo-type="dijit/MenuItem"
-                             data-dojo-props="onClick: function(e){qpidManagementHelper.showQueryBrowser(e);}">
-                            Browse
-                        </div>
-                    </div>
-                </div>
-                <div data-dojo-type="dijit/form/DropDownButton"
-                     data-dojo-props="iconClass: 'dijitIconDocuments'">
-                    <span>Documentation</span>
-                    <div data-dojo-type="dijit/Menu">
-                        <div data-dojo-type="dijit/MenuItem"
-                             data-dojo-props="onClick: function(){qpidManagementHelper.showAPI();}">API documentation
-                        </div>
-                        <div data-dojo-type="dijit/MenuItem"
-                             data-dojo-props="onClick: function(){qpidManagementHelper.showDocumentation();}">
-                            Broker Manual
+    <div id="loadingLayout">
+        <div class="header"></div>
+        <div class="clear"></div>
+        <div class="loadingBar">
+            <div>Please wait.... <span class="loading">&nbsp;&nbsp;&nbsp;&nbsp;</span> </div>
+        </div>
+
+    </div>
+
+    <div id="loginLayout" class="dijitHidden">
+        <div class="header"></div>
+        <div class="clear"></div>
+        <div style="width:350px; margin-left: auto; margin-right: auto;" id="loginForm"
+             data-dojo-type="qpid.common.LoginForm"></div>
+        <div qpid-type="footer"></div>
+    </div>
+
+    <div id="pageLayout" class="dijitHidden"
+         data-dojo-type="dijit.layout.BorderContainer"
+         data-dojo-props="design: 'headline', gutters: false, isLayoutContainer:false">
+        <div data-dojo-type="dijit.layout.ContentPane" data-dojo-props="region:'top', isLayoutContainer:false">
+            <div class="header"></div>
+            <div class="alignRight">
+                <div id="login" class="hidden" data-dojo-type="dijit/Toolbar">
+                    <div data-dojo-type="dijit/form/DropDownButton" data-dojo-props="iconClass: 'dijitIconChart'">
+                        <span>Dashboard</span>
+                        <div data-dojo-type="dijit/Menu">
+                            <div data-dojo-type="dijit/MenuItem"
+                                 data-dojo-props="onClick: function(e){qpidManagementHelper.showDashboardCreateDialog(e);}">
+                                Create
+                            </div>
+                            <div data-dojo-type="dijit/MenuItem"
+                                 data-dojo-props="onClick: function(e){qpidManagementHelper.showDashboardBrowser(e);}">
+                                Browse
+                            </div>
                         </div>
                     </div>
-                </div>
-                <span data-dojo-type="dijit/ToolbarSeparator"></span>
-                <div id="authenticatedUserControls" data-dojo-type="dijit/form/DropDownButton"
-                     data-dojo-props="iconClass: 'userIcon ui-icon'">
-                    <div data-dojo-type="dijit/Menu">
-                        <div data-dojo-type="dijit/MenuItem"
-                             data-dojo-props="onClick: function(){qpidManagementHelper.logout();}">Log out
+                    <div data-dojo-type="dijit/form/DropDownButton"
+                         data-dojo-props="iconClass: 'dijitIconApplication'">
+                        <span>Query</span>
+                        <div data-dojo-type="dijit/Menu">
+                            <div data-dojo-type="dijit/MenuItem"
+                                 data-dojo-props="onClick: function(e){qpidManagementHelper.showQueryCreateDialog(e);}">
+                                Create
+                            </div>
+                            <div data-dojo-type="dijit/MenuItem"
+                                 data-dojo-props="onClick: function(e){qpidManagementHelper.showQueryBrowser(e);}">
+                                Browse
+                            </div>
                         </div>
-                        <div data-dojo-type="dijit/MenuItem"
-                             data-dojo-props="onClick: function(){qpidManagementHelper.showPreferencesDialog();}">
-                            Preferences
+                    </div>
+                    <div data-dojo-type="dijit/form/DropDownButton"
+                         data-dojo-props="iconClass: 'dijitIconDocuments'">
+                        <span>Documentation</span>
+                        <div data-dojo-type="dijit/Menu">
+                            <div data-dojo-type="dijit/MenuItem"
+                                 data-dojo-props="onClick: function(){qpidManagementHelper.showAPI();}">API
+                                documentation
+                            </div>
+                            <div data-dojo-type="dijit/MenuItem"
+                                 data-dojo-props="onClick: function(){qpidManagementHelper.showDocumentation();}">
+                                Broker Manual
+                            </div>
+                        </div>
+                    </div>
+                    <span data-dojo-type="dijit/ToolbarSeparator"></span>
+                    <div id="authenticatedUserControls" data-dojo-type="dijit/form/DropDownButton"
+                         data-dojo-props="iconClass: 'userIcon ui-icon'">
+                        <div data-dojo-type="dijit/Menu">
+                            <div data-dojo-type="dijit/MenuItem"
+                                 data-dojo-props="onClick: function(){qpidManagementHelper.logout();}">Log out
+                            </div>
+                            <div data-dojo-type="dijit/MenuItem"
+                                 data-dojo-props="onClick: function(){qpidManagementHelper.showPreferencesDialog();}">
+                                Preferences
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div id="tree" data-dojo-type="dijit.layout.ContentPane"
-         data-dojo-props="region:'leading', splitter: true, isLayoutContainer:false">
-        <div qpid-type="treeView"></div>
-    </div>
-    <div id="main" data-dojo-type="dijit.layout.ContentPane"
-         data-dojo-props="region:'center', gutters: true, isLayoutContainer:false">
-        <div id="managedViews" data-dojo-type="dijit.layout.TabContainer"
-             data-dojo-props="tabPosition: 'top', doLayout:false, isLayoutContainer:false">
+        <div id="tree" data-dojo-type="dijit.layout.ContentPane"
+             data-dojo-props="region:'leading', splitter: true, isLayoutContainer:false">
+            <div qpid-type="treeView"></div>
         </div>
-        <div>
-            <div qpid-type="footer"></div>
+        <div id="main" data-dojo-type="dijit.layout.ContentPane"
+             data-dojo-props="region:'center', gutters: true, isLayoutContainer:false">
+            <div id="managedViews" data-dojo-type="dijit.layout.TabContainer"
+                 data-dojo-props="tabPosition: 'top', doLayout:false, isLayoutContainer:false">
+            </div>
+            <div>
+                <div qpid-type="footer"></div>
+            </div>
         </div>
     </div>
+
     <div class="dijitHidden">
         <div data-dojo-type="dijit.Dialog" data-dojo-props="title: 'Error'" id="errorDialog">
             <div>
@@ -215,7 +238,6 @@
             </div>
         </div>
     </div>
-</div>
 
 </body>
 </html>

broker-plugins/management-http/src/main/java/resources/js/qpid/common/ConsoleHelper.js
@@ -26,7 +26,9 @@ define(["dojo/query",
         "qpid/management/Management",
         "qpid/common/util",
         "dijit/Dialog",
-        "dojo/domReady!"], function (query, registry, entities, Structure, updater, Management, util, Dialog)
+        "dojo/dom-class",
+        "dojo/dom",
+        "dojo/domReady!"], function (query, registry, entities, Structure, updater, Management, util, Dialog, domClass, dom)
 {
 
     var documentationUrl = null;
@@ -177,42 +179,64 @@ define(["dojo/query",
             this.management = new Management("", util.xhrErrorHandler);
             this.structure = new Structure();
 
-            var that = this;
             var management = this.management;
             var structure = this.structure;
 
-            var authenticationSuccessCallback = function (data)
+            var authenticationSuccessCallback = function ()
             {
+                domClass.add(dom.byId("loginLayout"), "dijitHidden");
+
+                var pageLagoutContainer = registry.byId("pageLayout");
+                domClass.remove(pageLagoutContainer.domNode, "dijitHidden")
+                pageLagoutContainer.resize();
+
+                var controlButton = registry.byId("authenticatedUserControls");
+                registry.byId("login").domNode.style.display = "inline";
+                management.init(function ()
+                {
+                    updater.registerUpdateIntervalListener(management.userPreferences);
+                    var treeView = new TreeView(management, query('div[qpid-type="treeView"]')[0]);
+                    controller.init(management, structure, treeView);
+                    dijit.Tooltip.defaultPosition =
+                        ["after-centered",
+                         "below-centered"];
+                    if (controlButton)
+                    {
+                        var userName = management.getAuthenticatedUser();
+                        controlButton.set("label", util.toFriendlyUserName(userName));
+                        controlButton.set("title", userName);
+                        controlButton.domNode.style.display = '';
+                    }
+                });
+            };
+
+            this.management.getSaslStatus().then(function (data)
+            {
+                domClass.add(dom.byId("loadingLayout"), "dijitHidden");
                 if (data.user)
                 {
-                    var controlButton = registry.byId("authenticatedUserControls");
-                    registry.byId("login").domNode.style.display = "inline";
-                    management.init(function ()
-                    {
-                        updater.registerUpdateIntervalListener(management.userPreferences);
-                        var treeView = new TreeView(management, query('div[qpid-type="treeView"]')[0]);
-                        controller.init(management, structure, treeView);
-                        dijit.Tooltip.defaultPosition =
-                            ["after-centered",
-                             "below-centered"];
-                        if (controlButton)
-                        {
-                            var userName = management.getAuthenticatedUser();
-                            controlButton.set("label", util.toFriendlyUserName(userName));
-                            controlButton.set("title", userName);
-                            controlButton.domNode.style.display = '';
-                        }
-                    });
+                    authenticationSuccessCallback();
                 }
                 else
                 {
-                    alert("User identifier is not found! Re-authenticate!");
-                    that.logout();
+                    domClass.remove(dom.byId("loginLayout"), "dijitHidden");
+                    var loginForm = registry.byId("loginForm");
+                    loginForm.on("submit", function (credentials)
+                    {
+                        management.authenticate(data.mechanisms, credentials)
+                            .then(function ()
+                                {
+                                    loginForm.hide();
+                                    authenticationSuccessCallback();
+                                },
+                                function (error)
+                                {
+                                    loginForm.onError(error);
+                                });
+                    });
+                    loginForm.show();
                 }
-            };
-
-            management.authenticate()
-                .then(authenticationSuccessCallback);
+            }, util.xhrErrorHandler);
         },
         logout: function ()
         {

broker-plugins/management-http/src/main/java/resources/js/qpid/common/LoginForm.js
@@ -1,0 +1,130 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+define(["dojo/_base/declare",
+        "dojo/_base/lang",
+        "dojo/dom-class",
+        "dojo/text!common/LoginForm.html",
+        "dojo/Evented",
+        "dojox/html/entities",
+        "dijit/_WidgetBase",
+        "dijit/_TemplatedMixin",
+        "dijit/_WidgetsInTemplateMixin",
+        "dijit/TitlePane",
+        "dijit/form/Form",
+        "dijit/form/ValidationTextBox",
+        "dijit/form/Button",
+        "dojox/layout/TableContainer",
+        "dojo/domReady!"],
+    function (declare,
+              lang,
+              domClass,
+              template,
+              Evented,
+              entities)
+    {
+
+        return declare("qpid.common.LoginForm",
+                       [dijit._WidgetBase, dijit._TemplatedMixin, dijit._WidgetsInTemplateMixin, Evented],
+            {
+
+            //Strip out the apache comment header from the template html as comments unsupported.
+            templateString: template.replace(/<!--[\s\S]*?-->/g, ""),
+
+            // template fields
+            formWidget: null,
+            usernameWidget: null,
+            passwordWidget: null,
+            loginButtonWidget: null,
+            statusCodeNode: null,
+            errorMessageNode: null,
+
+            //
+
+            postCreate: function ()
+            {
+                this.inherited(arguments);
+                this._postCreate();
+            },
+            _postCreate: function ()
+            {
+                this.formWidget.on("submit", lang.hitch(this, this._onFormSubmit));
+                this.usernameWidget.on("change", lang.hitch(this, this._onCredentialChange));
+                this.passwordWidget.on("change", lang.hitch(this, this._onCredentialChange));
+            },
+            show: function()
+            {
+                domClass.remove(this.domNode, "dijitHidden");
+            },
+            hide: function()
+            {
+                domClass.add(this.domNode, "dijitHidden");
+            },
+            onError: function (error)
+            {
+                if (error.response)
+                {
+                    this.statusCodeNode.innerHTML =  entities.encode(String(error.response.status));
+                    if (error.response.status == 401)
+                    {
+                        this.errorMessageNode.innerHTML = "Authentication Failed";
+                    }
+                    else if (error.response.status == 403)
+                    {
+                        this.errorMessageNode.innerHTML ="Authorization Failed";
+                    }
+                    else
+                    {
+                        this.errorMessageNode.innerHTML =  entities.encode(String(error.message));
+                    }
+                }
+                else
+                {
+                    var message = error.message ?  entities.encode(String(error.message)) : "Authentication failed";
+                    this.statusCodeNode.innerHTML = "";
+                    this.errorMessageNode.innerHTML = message;
+                }
+                this.usernameWidget.set("disabled", false);
+                this.passwordWidget.set("disabled", false);
+                this.formWidget.reset();
+                this.usernameWidget.focus();
+            },
+            _onFormSubmit: function (event)
+            {
+                event.preventDefault();
+                if (this.formWidget.validate())
+                {
+                    var data = {username: this.usernameWidget.value, password: this.passwordWidget.value};
+                    this.emit("submit", data);
+
+                    this.usernameWidget.set("disabled", true);
+                    this.passwordWidget.set("disabled", true);
+                    this.loginButtonWidget.set("disabled", true);
+                }
+                return false;
+            },
+            _onCredentialChange: function ()
+            {
+                this.loginButtonWidget.set("disabled", !this.usernameWidget.value || !this.passwordWidget.value);
+            }
+
+        });
+    });

commit 135fc404db42ee4a59c98a33671965cfb8409e42

QPID-7789: Fix defect affecting Oauth2 authentication introduced by commit 6bde48c519847a14dcdbeba33b9eb200f3a089cd

broker-plugins/management-http/src/main/java/org/apache/qpid/server/management/plugin/HttpManagement.java
@@ -347,6 +347,7 @@ public class HttpManagement extends AbstractPluginAdapter<HttpManagement> implem
         root.addFilter(restAuthorizationFilter, "/apidocs/*", EnumSet.of(DispatcherType.REQUEST));
         root.addFilter(restAuthorizationFilter, "/service/*", EnumSet.of(DispatcherType.REQUEST));
 
+        root.addFilter(new FilterHolder(new RedirectingFilter()), "/index.html", EnumSet.of(DispatcherType.REQUEST));
         root.addFilter(new FilterHolder(new RedirectingFilter()), "/", EnumSet.of(DispatcherType.REQUEST));
         if (_serveUncompressedDojo)
         {

broker-plugins/management-http/src/main/java/org/apache/qpid/server/management/plugin/auth/UsernamePasswordInteractiveLogin.java
@@ -22,6 +22,7 @@ package org.apache.qpid.server.management.plugin.auth;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -34,20 +35,7 @@ import org.apache.qpid.server.security.auth.manager.UsernamePasswordAuthenticati
 @PluggableService
 public class UsernamePasswordInteractiveLogin implements HttpRequestInteractiveAuthenticator
 {
-    // TODO: When we refactor web management and adopt web fragments, move logout.html
-    // to WEB-INF/ and dispatch (forward) to them, rather than using a client side redirect.
-    // This would keep the login/logout pages private and inaccessible to the user when using auth providers
-    // such as Ouath2.
     private static final String DEFAULT_LOGIN_URL = "/index.html";
-
-    private static final AuthenticationHandler REDIRECT_HANDLER = new AuthenticationHandler()
-    {
-        @Override
-        public void handleAuthentication(final HttpServletResponse response) throws IOException
-        {
-            response.sendRedirect(DEFAULT_LOGIN_URL);
-        }
-    };
 
     private static  final LogoutHandler LOGOUT_HANDLER = new LogoutHandler()
     {
@@ -64,7 +52,7 @@ public class UsernamePasswordInteractiveLogin implements HttpRequestInteractiveA
     {
         if(configuration.getAuthenticationProvider(request) instanceof UsernamePasswordAuthenticationProvider)
         {
-            return REDIRECT_HANDLER;
+            return response -> request.getRequestDispatcher(DEFAULT_LOGIN_URL).forward(request, response);
         }
         else
         {

broker-plugins/management-http/src/main/java/org/apache/qpid/server/management/plugin/filter/RedirectingFilter.java
@@ -40,7 +40,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.qpid.server.management.plugin.HttpManagementConfiguration;
 import org.apache.qpid.server.management.plugin.HttpManagementUtil;
 import org.apache.qpid.server.management.plugin.HttpRequestInteractiveAuthenticator;
-import org.apache.qpid.server.model.Broker;
 import org.apache.qpid.server.plugin.QpidServiceLoader;
 import org.apache.qpid.server.security.auth.AuthenticatedPrincipal;
 