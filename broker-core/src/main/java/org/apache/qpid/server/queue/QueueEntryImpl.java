apache/qpid-broker-j

commit 0406c87342ddf296ff9d4b4697923c1d397b9ed3

QPID-7889: [Java Broker] [AMQP 0-91] On basic.nack, release the message for delivery elsewhere

broker-core/src/main/java/org/apache/qpid/server/queue/QueueEntryImpl.java
@@ -20,8 +20,9 @@
  */
 package org.apache.qpid.server.queue;
 
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 import java.util.concurrent.atomic.AtomicLongFieldUpdater;
@@ -39,8 +40,6 @@ import org.apache.qpid.server.message.MessageInstanceConsumer;
 import org.apache.qpid.server.message.MessageReference;
 import org.apache.qpid.server.message.RoutingResult;
 import org.apache.qpid.server.message.ServerMessage;
-import org.apache.qpid.server.model.AlternateBinding;
-import org.apache.qpid.server.model.Exchange;
 import org.apache.qpid.server.model.Queue;
 import org.apache.qpid.server.store.MessageEnqueueRecord;
 import org.apache.qpid.server.store.TransactionLogResource;
@@ -58,7 +57,9 @@ public abstract class QueueEntryImpl implements QueueEntry
 
     private final MessageReference _message;
 
-    private Set<Object> _rejectedBy = null;
+    private volatile Set<Object> _rejectedBy = null;
+    private static final AtomicReferenceFieldUpdater<QueueEntryImpl, Set> _rejectedByUpdater =
+            AtomicReferenceFieldUpdater.newUpdater(QueueEntryImpl.class, Set.class, "_rejectedBy");
 
     private static final EntryState HELD_STATE = new EntryState()
     {
@@ -493,23 +494,18 @@ public abstract class QueueEntryImpl implements QueueEntry
     }
 
     @Override
-    public void reject()
+    public void reject(final MessageInstanceConsumer<?> consumer)
     {
-        QueueConsumer<?,?> consumer = getAcquiringConsumer();
-
-        if (consumer != null)
+        if (consumer == null)
         {
-            if (_rejectedBy == null)
-            {
-                _rejectedBy = new HashSet<>();
-            }
+            throw new IllegalArgumentException("consumer must not be null");
+        }
 
-            _rejectedBy.add(consumer.getIdentifier());
-        }
-        else
+        if (_rejectedBy == null)
         {
-            _log.warn("Requesting rejection by null subscriber:" + this);
+            _rejectedByUpdater.compareAndSet(this, null, Collections.newSetFromMap(new ConcurrentHashMap<>()));
         }
+        _rejectedBy.add(consumer);
     }
 
     @Override

broker-core/src/main/java/org/apache/qpid/server/message/MessageInstance.java
@@ -57,7 +57,7 @@ public interface MessageInstance
 
     boolean isRedelivered();
 
-    void reject();
+    void reject(final MessageInstanceConsumer<?> consumer);
 
     boolean isRejectedBy(MessageInstanceConsumer consumer);
 

broker-core/src/main/java/org/apache/qpid/server/virtualhost/AbstractSystemMessageSource.java
@@ -296,7 +296,7 @@ public abstract class AbstractSystemMessageSource implements MessageSource
         }
 
         @Override
-        public void reject()
+        public void reject(final MessageInstanceConsumer<?> consumer)
         {
             delete();
         }

broker-core/src/test/java/org/apache/qpid/server/queue/MockMessageInstance.java
@@ -146,7 +146,7 @@ public class MockMessageInstance implements MessageInstance
     }
 
     @Override
-    public void reject()
+    public void reject(final MessageInstanceConsumer<?> consumer)
     {
     }
 

broker-core/src/test/java/org/apache/qpid/server/queue/QueueEntryImplTestBase.java
@@ -261,7 +261,7 @@ public abstract class QueueEntryImplTestBase extends QpidTestCase
 
         //acquire, reject, and release the message using the consumer
         assertTrue("Queue entry should have been able to be acquired", _queueEntry.acquire(sub));
-        _queueEntry.reject();
+        _queueEntry.reject(sub);
         _queueEntry.release();
 
         //verify the rejection is recorded
@@ -272,7 +272,7 @@ public abstract class QueueEntryImplTestBase extends QpidTestCase
 
         assertFalse("Queue entry should not yet have been rejected by the consumer", _queueEntry.isRejectedBy(sub2));
         assertTrue("Queue entry should have been able to be acquired", _queueEntry.acquire(sub2));
-        _queueEntry.reject();
+        _queueEntry.reject(sub2);
 
         //verify it still records being rejected by both consumers
         assertTrue("Queue entry should have been rejected by the consumer", _queueEntry.isRejectedBy(sub));

broker-plugins/amqp-0-8-protocol/src/main/java/org/apache/qpid/server/protocol/v0_8/AMQChannel.java
@@ -2332,7 +2332,7 @@ public class AMQChannel extends AbstractAMQPSession<AMQChannel, ConsumerTarget_0
                     }
                     else
                     {
-                        message.reject();
+                        message.reject(unackedMessageConsumerAssociation.getConsumer());
 
                         final boolean maxDeliveryCountEnabled = isMaxDeliveryCountEnabled(deliveryTag);
                         if (_logger.isDebugEnabled())
@@ -2359,6 +2359,7 @@ public class AMQChannel extends AbstractAMQPSession<AMQChannel, ConsumerTarget_0
                             else
                             {
                                 message.incrementDeliveryCount();
+                                message.release(unackedMessageConsumerAssociation.getConsumer());
                             }
                         }
                         else

broker-plugins/management-amqp/src/main/java/org/apache/qpid/server/management/amqp/ManagementNode.java
@@ -1684,7 +1684,7 @@ class ManagementNode implements MessageSource, MessageDestination, BaseQueue
         }
 
         @Override
-        public void reject()
+        public void reject(final MessageInstanceConsumer<?> consumer)
         {
 
         }

broker-plugins/management-amqp/src/main/java/org/apache/qpid/server/management/amqp/ManagementResponse.java
@@ -125,7 +125,7 @@ class ManagementResponse implements MessageInstance
     }
 
     @Override
-    public void reject()
+    public void reject(final MessageInstanceConsumer<?> consumer)
     {
         delete();
     }

commit 764d9b16d3d5c4d51b48a85c4e4fa4934e56e556

QPID-7889: Fix test failure introduced by 0406c87342ddf296ff9d4b4697923c1d397b9ed3

broker-core/src/main/java/org/apache/qpid/server/queue/QueueEntryImpl.java
@@ -505,7 +505,7 @@ public abstract class QueueEntryImpl implements QueueEntry
         {
             _rejectedByUpdater.compareAndSet(this, null, Collections.newSetFromMap(new ConcurrentHashMap<>()));
         }
-        _rejectedBy.add(consumer);
+        _rejectedBy.add(consumer.getIdentifier());
     }
 
     @Override