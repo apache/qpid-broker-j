6845fa67cccc595ebdffa6793501e52f79d8e93d
Keith Wall <kwall@apache.org>
2017-09-15 09:30:16 +0100

    Revert "QPID-7773: [Java Broker, REST API] Fix REST API calls in QpidRestAPIQueueCreator"
    This reverts commit 99365c079bb9571cd2fd9fe02a8dd134949ebca8.

--- perftests/src/main/java/org/apache/qpid/disttest/jms/QpidRestAPIQueueCreator.java
+++ perftests/src/main/java/org/apache/qpid/disttest/jms/QpidRestAPIQueueCreator.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -66,7 +67,7 @@ public class QpidRestAPIQueueCreator implements QueueCreator
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(QpidRestAPIQueueCreator.class);
     private static int _drainPollTimeout = Integer.getInteger(QUEUE_CREATOR_DRAIN_POLL_TIMEOUT, 500);
-    private static final TypeReference<HashMap<String, Object>> MAP_TYPE_REFERENCE = new TypeReference<HashMap<String,Object>>(){};
+    private static final TypeReference<List<HashMap<String, Object>>> MAP_TYPE_REFERENCE = new TypeReference<List<HashMap<String,Object>>>(){};
 
     private final HttpHost _management;
     private final String _virtualhostnode;
@@ -243,7 +244,8 @@ public class QpidRestAPIQueueCreator implements QueueCreator
     private Map<String, Object> managementQueryBroker(final HttpClientContext context)
     {
         HttpGet get = new HttpGet(_brokerApiUrl);
-        return executeManagement(get, context);
+        final List<Map<String, Object>> maps = executeManagement(get, context);
+        return maps.isEmpty() ? Collections.emptyMap() : maps.get(0);
     }
 
     private void managementCreateQueue(final String name, final HttpClientContext context)
@@ -263,7 +265,7 @@ public class QpidRestAPIQueueCreator implements QueueCreator
         executeManagement(delete, context);
     }
 
-    private Map<String, Object> executeManagement(final HttpRequest httpRequest, final HttpClientContext context)
+    private List<Map<String, Object>> executeManagement(final HttpRequest httpRequest, final HttpClientContext context)
     {
         try(CloseableHttpClient httpClient = HttpClients.custom()
                                                         .setDefaultCredentialsProvider(_credentialsProvider)
