91e14a64da924caa654b8c69a0f27e4c2e694342
Keith Wall <kwall@apache.org>
2018-02-15 20:41:49 +0000

    QPID-8083 [System Tests] [REST/HTTP] Fix test failure that would occur if working path too long

--- systests/qpid-systests-http-management/src/test/java/org/apache/qpid/tests/http/rest/model/ReadTest.java
+++ systests/qpid-systests-http-management/src/test/java/org/apache/qpid/tests/http/rest/model/ReadTest.java
@@ -277,7 +277,7 @@ public class ReadTest extends HttpTestBase
                 final Map<String, Object> attrs = new HashMap<>(base);
                 attrs.put(NonJavaKeyStore.TYPE, "NonJavaKeyStore");
                 attrs.put(NonJavaKeyStore.PRIVATE_KEY_URL, privateKeyFileUrl);
-                getHelper().submitRequest(storeUrl, "PUT", attrs, SC_CREATED);
+                getHelper().submitRequest(String.format("%s?oversize=%d", storeUrl, storeUrl.length()), "PUT", attrs, SC_CREATED);
 
                 final Map<String, Object> store = getHelper().getJsonAsMap(storeUrl);
                 assertThat(store.get(NonJavaKeyStore.PRIVATE_KEY_URL), is(equalTo(privateKeyFileUrl)));
