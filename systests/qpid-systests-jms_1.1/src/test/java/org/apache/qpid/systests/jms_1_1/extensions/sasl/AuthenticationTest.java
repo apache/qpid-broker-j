6c89754356b24a6b90313749f9659feba80a90fd
Tomas Vavricka <tomas.vavricka@deutsche-boerse.com>
2020-01-17 15:26:27 +0100

    QPID-8367 Improved variable names in AuthenticationTest

--- systests/qpid-systests-jms_1.1/src/test/java/org/apache/qpid/systests/jms_1_1/extensions/sasl/AuthenticationTest.java
+++ systests/qpid-systests-jms_1.1/src/test/java/org/apache/qpid/systests/jms_1_1/extensions/sasl/AuthenticationTest.java
@@ -31,6 +31,7 @@ import static org.junit.Assume.assumeThat;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -79,7 +80,7 @@ public class AuthenticationTest extends JmsTestBase
     // see how port is specified when certificates are generated in script
     // test-profiles/test_resources/ssl/generate_certificates.sh
     private static final int CRL_HTTP_PORT = 8186;
-    private static HttpServer _server;
+    private static HttpServer _crlServer;
 
     @BeforeClass
     public static void setUp() throws IOException
@@ -98,12 +99,12 @@ public class AuthenticationTest extends JmsTestBase
             System.setProperty("javax.net.ssl.trustStoreType", TestSSLConstants.JAVA_KEYSTORE_TYPE);
             System.setProperty("javax.net.ssl.keyStoreType", TestSSLConstants.JAVA_KEYSTORE_TYPE);
         }
-        _server = HttpServer.create(new InetSocketAddress(CRL_HTTP_PORT), 0);
+        _crlServer = HttpServer.create(new InetSocketAddress(CRL_HTTP_PORT), 0);
         createContext(Paths.get(TestSSLConstants.CA_CRL));
         createContext(Paths.get(TestSSLConstants.CA_CRL_EMPTY));
         createContext(Paths.get(TestSSLConstants.INTERMEDIATE_CA_CRL));
-        _server.setExecutor(null);
-        _server.start();
+        _crlServer.setExecutor(null);
+        _crlServer.start();
     }
 
     @AfterClass
@@ -120,7 +121,7 @@ public class AuthenticationTest extends JmsTestBase
             System.clearProperty("javax.net.ssl.trustStoreType");
             System.clearProperty("javax.net.ssl.keyStoreType");
         }
-        _server.stop(0);
+        _crlServer.stop(0);
     }
 
 
@@ -727,25 +728,25 @@ public class AuthenticationTest extends JmsTestBase
 
     private static void createContext(Path crlPath)
     {
-        _server.createContext("/" + crlPath.getFileName(), new Handler(crlPath));
+        _crlServer.createContext("/" + crlPath.getFileName(), new CrlServerHandler(crlPath));
     }
 
-    private static class Handler implements HttpHandler
+    private static class CrlServerHandler implements HttpHandler
     {
         final Path crlPath;
-        public Handler(Path crlPath)
+        public CrlServerHandler(Path crlPath)
         {
             this.crlPath = crlPath;
         }
 
         @Override
-        public void handle(HttpExchange t) throws IOException
+        public void handle(HttpExchange httpExchange) throws IOException
         {
-            byte[] bytes = Files.readAllBytes(crlPath);
-            t.sendResponseHeaders(200, bytes.length);
-            OutputStream os = t.getResponseBody();
-            os.write(bytes);
-            os.close();
+            final byte[] crlBytes = Files.readAllBytes(crlPath);
+            httpExchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, crlBytes.length);
+            final OutputStream responseBody = httpExchange.getResponseBody();
+            responseBody.write(crlBytes);
+            responseBody.close();
         }
     }
 }
