0279164b46ad018ebc17387380ba46d1a96ba238
Alex Rudyy <orudyy@apache.org>
2020-03-02 16:55:06 +0000

    NO-JIRA: Replace org.junit.Assert.assertThat with org.hamcrest.MatcherAssert.assertThat;

--- broker-core/src/test/java/org/apache/qpid/server/virtualhost/connection/ConnectionPrincipalStatisticsRegistryImplTest.java
+++ broker-core/src/test/java/org/apache/qpid/server/virtualhost/connection/ConnectionPrincipalStatisticsRegistryImplTest.java
@@ -21,7 +21,7 @@ package org.apache.qpid.server.virtualhost.connection;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
