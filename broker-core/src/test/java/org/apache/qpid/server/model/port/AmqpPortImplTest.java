0293ab1513d33d7ec37f6c6868ea0d3fdf188566
Alex Rudyy <orudyy@apache.org>
2020-09-12 22:56:11 +0100

    NO-JIRA: Replace deprecated Assert.assertThat with MatcherAssert.assertThat

--- broker-core/src/test/java/org/apache/qpid/server/model/port/AmqpPortImplTest.java
+++ broker-core/src/test/java/org/apache/qpid/server/model/port/AmqpPortImplTest.java
@@ -21,9 +21,9 @@ package org.apache.qpid.server.model.port;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
--- broker-core/src/test/java/org/apache/qpid/server/security/auth/manager/oauth2/OAuth2AuthenticationProviderImplTest.java
+++ broker-core/src/test/java/org/apache/qpid/server/security/auth/manager/oauth2/OAuth2AuthenticationProviderImplTest.java
@@ -22,8 +22,8 @@ package org.apache.qpid.server.security.auth.manager.oauth2;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
