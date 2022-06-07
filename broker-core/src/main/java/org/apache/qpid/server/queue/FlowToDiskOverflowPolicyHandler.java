2774b743fd6b83a74b3368be87072ab62ba2ec4a
dakirily <daniel.kirilyuk@gmail.com>
2022-06-07 08:25:09 +0200

    QPID-8590: [Broker-J] Purge on flow to disk queue

--- broker-core/src/main/java/org/apache/qpid/server/queue/FlowToDiskOverflowPolicyHandler.java
+++ broker-core/src/main/java/org/apache/qpid/server/queue/FlowToDiskOverflowPolicyHandler.java
@@ -18,54 +18,96 @@
  */
 package org.apache.qpid.server.queue;
 
+import org.apache.qpid.server.bytebuffer.QpidByteBuffer;
 import org.apache.qpid.server.message.MessageDeletedException;
 import org.apache.qpid.server.message.MessageReference;
 import org.apache.qpid.server.message.ServerMessage;
 import org.apache.qpid.server.model.OverflowPolicy;
 import org.apache.qpid.server.model.Queue;
+import org.apache.qpid.server.store.StoredMessage;
 
+/**
+ * If the queue breaches a limit, newly arriving messages are written to disk and the in-memory representation of
+ * the message is minimised. The Broker will transparently retrieve messages from disk as they are required by a
+ * consumer or management. The flow to disk policy does not actually restrict the overall size of the queue,
+ * merely the space occupied in memory.
+ */
 public class FlowToDiskOverflowPolicyHandler implements OverflowPolicyHandler
 {
+    /**
+     * Delegate handler
+     */
     private final Handler _handler;
 
+    /**
+     * Constructor injects queue
+     *
+     * @param queue Queue
+     */
     FlowToDiskOverflowPolicyHandler(final Queue<?> queue)
     {
         _handler = new Handler(queue);
         queue.addChangeListener(_handler);
     }
 
+    /**
+     * Checks queue overflow (is called when adding or deleting queue entry)
+     *
+     * @param newlyEnqueued QueueEntry enqueued (is null on deletion)
+     */
     @Override
     public void checkOverflow(final QueueEntry newlyEnqueued)
     {
-        _handler.checkOverflow(newlyEnqueued);
-
+        _handler.checkOverflow(newlyEnqueued, true);
     }
 
+    /**
+     * Delegate handler
+     */
     private static class Handler extends OverflowPolicyMaximumQueueDepthChangeListener
     {
+        /**
+         * Queue instance
+         */
         private final Queue<?> _queue;
 
+        /**
+         * Constructor injects queue
+         *
+         * @param queue Queue
+         */
         private Handler(final Queue<?> queue)
         {
             super(OverflowPolicy.FLOW_TO_DISK);
             _queue = queue;
         }
 
+        /**
+         * Is called when max queue depth is changed
+         *
+         * @param queue Queue
+         */
         @Override
         void onMaximumQueueDepthChange(final Queue<?> queue)
         {
-            checkOverflow(null);
+            checkOverflow(null, false);
         }
 
-        private void checkOverflow(final QueueEntry newlyEnqueued)
+        /**
+         * Either flows messages to the disk or restores them into the memory
+         *
+         * @param newlyEnqueued QueueEntry (could be null in case of deletion or limit change)
+         * @param stopOnFirstMatch Whether flowing to disk / restoring to memory should be stopped after fist match
+         */
+        private void checkOverflow(final QueueEntry newlyEnqueued, final boolean stopOnFirstMatch)
         {
-            long maximumQueueDepthBytes = _queue.getMaximumQueueDepthBytes();
-            long maximumQueueDepthMessages = _queue.getMaximumQueueDepthMessages();
+            final long maximumQueueDepthBytes = _queue.getMaximumQueueDepthBytes();
+            final long maximumQueueDepthMessages = _queue.getMaximumQueueDepthMessages();
             if (maximumQueueDepthBytes >= 0L || maximumQueueDepthMessages >= 0L)
             {
                 if (newlyEnqueued == null)
                 {
-                    flowTailToDiskIfNecessary(maximumQueueDepthBytes, maximumQueueDepthMessages);
+                    balanceTailIfNecessary(maximumQueueDepthBytes, maximumQueueDepthMessages, stopOnFirstMatch);
                 }
                 else
                 {
@@ -74,35 +116,58 @@ public class FlowToDiskOverflowPolicyHandler implements OverflowPolicyHandler
             }
         }
 
-        private void flowTailToDiskIfNecessary(final long maximumQueueDepthBytes, final long maximumQueueDepthMessages)
+        /**
+         * Either flows tail messages to disk or restores them into the memory depending on the overflow limit.
+         * Boolean flag stopOnFirstMatch is true when enqueueing or deleting messages, is false when overflow limit
+         * was changed
+         *
+         * @param maximumQueueDepthBytes Max queue depth in bytes
+         * @param maximumQueueDepthMessages Max queue depth in messages
+         * @param stopOnFirstMatch Whether flowing to disk / restoring to memory should be stopped after fist match
+         */
+        private void balanceTailIfNecessary(
+            final long maximumQueueDepthBytes,
+            final long maximumQueueDepthMessages,
+            final boolean stopOnFirstMatch)
         {
             final long queueDepthBytes = _queue.getQueueDepthBytes();
             final long queueDepthMessages = _queue.getQueueDepthMessages();
+            final boolean isMaximumQueueDepthBytesUnlimited = maximumQueueDepthBytes >= 0L;
 
-            if ((maximumQueueDepthBytes >= 0L && queueDepthBytes > maximumQueueDepthBytes) ||
-                (maximumQueueDepthMessages >= 0L && queueDepthMessages > maximumQueueDepthMessages))
+            if ((isMaximumQueueDepthBytesUnlimited && queueDepthBytes > maximumQueueDepthBytes) ||
+                (maximumQueueDepthMessages >= 0L && queueDepthMessages >= maximumQueueDepthMessages))
             {
 
                 long cumulativeDepthBytes = 0;
                 long cumulativeDepthMessages = 0;
 
-                QueueEntryIterator queueEntryIterator = _queue.queueEntryIterator();
+                final QueueEntryIterator queueEntryIterator = _queue.queueEntryIterator();
                 while (queueEntryIterator.advance())
                 {
-                    QueueEntry node = queueEntryIterator.getNode();
+                    final QueueEntry node = queueEntryIterator.getNode();
 
                     if (node != null && !node.isDeleted())
                     {
-                        ServerMessage message = node.getMessage();
+                        final ServerMessage<?> message = node.getMessage();
                         if (message != null)
                         {
                             cumulativeDepthMessages++;
                             cumulativeDepthBytes += message.getSizeIncludingHeader();
 
-                            if (cumulativeDepthBytes > maximumQueueDepthBytes
+                            final boolean isInMemory = message.getStoredMessage().isInContentInMemory();
+
+                            if ((isMaximumQueueDepthBytesUnlimited && cumulativeDepthBytes > maximumQueueDepthBytes)
                                 || cumulativeDepthMessages > maximumQueueDepthMessages)
                             {
+                                if (stopOnFirstMatch || !isInMemory)
+                                {
+                                    break;
+                                }
                                 flowToDisk(node);
+                            }
+                            else if (!isInMemory)
+                            {
+                                restoreInMemory(node);
                             }
                         }
                     }
@@ -110,9 +175,17 @@ public class FlowToDiskOverflowPolicyHandler implements OverflowPolicyHandler
             }
         }
 
-        private void flowNewEntryToDiskIfNecessary(final QueueEntry newlyEnqueued,
-                                                   final long maximumQueueDepthBytes,
-                                                   final long maximumQueueDepthMessages)
+        /**
+         * Flows queue entry to the disk (when overflow limit is exceeded)
+         *
+         * @param newlyEnqueued QueueEntry
+         * @param maximumQueueDepthBytes Max queue depth in bytes
+         * @param maximumQueueDepthMessages Max queue depth in messages
+         */
+        private void flowNewEntryToDiskIfNecessary(
+            final QueueEntry newlyEnqueued,
+            final long maximumQueueDepthBytes,
+            final long maximumQueueDepthMessages)
         {
             final long queueDepthBytes = _queue.getQueueDepthBytes();
             final long queueDepthMessages = _queue.getQueueDepthMessages();
@@ -124,13 +197,42 @@ public class FlowToDiskOverflowPolicyHandler implements OverflowPolicyHandler
             }
         }
 
+        /**
+         * Flows queue entry to the disk
+         *
+         * @param node QueueEntry
+         */
         private void flowToDisk(final QueueEntry node)
         {
-            try (MessageReference messageReference = node.getMessage().newReference())
+            try (final MessageReference<?> messageReference = node.getMessage().newReference())
             {
                 if (node.getQueue().checkValid(node))
                 {
                     messageReference.getMessage().getStoredMessage().flowToDisk();
+                }
+            }
+            catch (MessageDeletedException mde)
+            {
+                // pass
+            }
+        }
+
+        /**
+         * Restores queue entry in memory
+         *
+         * @param node QueueEntry
+         */
+        private void restoreInMemory(final QueueEntry node)
+        {
+            try (final MessageReference<?> messageReference = node.getMessage().newReference())
+            {
+                if (node.getQueue().checkValid(node))
+                {
+                    final StoredMessage<?> storedMessage = messageReference.getMessage().getStoredMessage();
+                    try (final QpidByteBuffer qpidByteBuffer = storedMessage.getContent(0, storedMessage.getContentSize()))
+                    {
+                        qpidByteBuffer.dispose();
+                    }
                 }
             }
             catch (MessageDeletedException mde)
