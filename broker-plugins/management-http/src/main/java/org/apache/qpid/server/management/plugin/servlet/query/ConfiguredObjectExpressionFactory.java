26a5d57ef7daf8f03d6e5a2dd77a4b0acc49b327
Sudheer Sana <sudheer050.iitm@gmail.com>
2025-01-28 08:57:02 +0530

    QPID-8681: [Broker-J] Addressing lock contention in Sorted Queues under high load by optimizing property fetching

--- broker-plugins/management-http/src/main/java/org/apache/qpid/server/management/plugin/servlet/query/ConfiguredObjectExpressionFactory.java
+++ broker-plugins/management-http/src/main/java/org/apache/qpid/server/management/plugin/servlet/query/ConfiguredObjectExpressionFactory.java
@@ -42,6 +42,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TimeZone;
+import java.util.Collections;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
@@ -313,10 +314,12 @@ public class ConfiguredObjectExpressionFactory
     {
 
         private final String _propertyName;
+        private final List<String> _propertyList;
 
         public ConfiguredObjectPropertyExpression(final String propertyName)
         {
             _propertyName = propertyName;
+            _propertyList = Collections.singletonList(_propertyName);
         }
 
         @Override
@@ -343,7 +346,7 @@ public class ConfiguredObjectExpressionFactory
             {
                 return object.getAttributeNames().contains(_propertyName)
                         ? object.getAttribute(_propertyName)
-                        : object.getStatistics().get(_propertyName);
+                        : object.getStatistics(_propertyList).get(_propertyName);
             }
         }
 
